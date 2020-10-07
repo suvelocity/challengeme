89e1fe73fdac41cf02bb5ee1b9550bd2b8c23a85
RoyShnitzel <67857836+RoyShnitzel@users.noreply.github.com>
2020-10-07 16:21:52 +0300

    added challenges by success

--- server/api/v1/statisticsRoutes/insights.js
+++ server/api/v1/statisticsRoutes/insights.js
@@ -1,24 +1,44 @@
 const { Router } = require("express");
-const sequelize = require('sequelize')
+const sequelize = require("sequelize");
 const router = Router();
 const { Submission, Challenge } = require("../../../models");
 
 router.get("/top-challenges", async (req, res) => {
-    const sub = await Submission.findAll({
-      attributes: {
-        include: [
-          [sequelize.fn("COUNT", sequelize.col("challenge_id")), "count_sub"],
-        ],
-      },
-      include: {
-        model: Challenge,
-        attributes: ["name"],
-      },
-      group: ['challenge_id'],
-      order: [[sequelize.fn("COUNT", sequelize.col("challenge_id")), 'DESC']],
-      limit: 10,
-    });
-  
+  const sub = await Submission.findAll({
+    attributes: {
+      include: [
+        [sequelize.fn("COUNT", sequelize.col("challenge_id")), "count_sub"],
+      ],
+    },
+    include: {
+      model: Challenge,
+      attributes: ["name"],
+    },
+    group: ["challenge_id"],
+    order: [[sequelize.fn("COUNT", sequelize.col("challenge_id")), "DESC"]],
+    limit: 10,
+  });
+
+  res.json(sub);
+});
+
+router.get("/top-success", async (req, res) => {
+  const sub = await Submission.findAll({
+    attributes: {
+      include: [
+        [sequelize.fn("COUNT", sequelize.col("challenge_id")), "countSuc"],
+      ],
+    },
+    include: {
+      model: Challenge,
+      attributes: ["name"],
+    },
+    where: { state: "SUCCESS" },
+    group: ["challenge_id"],
+    order: [[sequelize.fn("COUNT", sequelize.col("challenge_id")), "DESC"]],
+    limit: 10,
+  });
+
   res.json(sub);
 });
 
