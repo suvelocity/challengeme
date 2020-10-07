638c31bcdece803a4cfaa56cd6d3ebe58f2294fb
ShaharEli <shahar.e3@gmail.com>
2020-10-07 10:17:10 +0300

    Update AddImg.js

--- client/src/components/AddImg.js
+++ client/src/components/AddImg.js
@@ -1,9 +1,4 @@
 import React,{useMemo, useState} from 'react'
-import {useDropzone} from 'react-dropzone'
-import Cropper from 'react-cropper';
-import 'cropperjs/dist/cropper.css';
-import ImageCropper from 'react-image-file-cropper';
-import 'react-image-file-cropper/cropper.css'; 
 import "./AddImg.css"
 import DropNCrop from '@synapsestudios/react-drop-n-crop';
 
@@ -22,8 +17,8 @@ const onChange = value => {
     return (
         <div className="upload-img-container">
             <div className="upload-img-secondary-container">
-            <DropNCrop cropperOptions={{aspectRatio:400/150}} onChange={onChange} value={file} />
-        <Button variant="contained" color="secondary" contained onClick={()=>setFile({})} >
+            <DropNCrop cropperOptions={{aspectRatio:400/150,height:200}} onChange={onChange} value={file} />
+        <Button variant="contained" color="secondary" contained onClick={()=>setFile("")} >
             Remove img
         </Button>
             </div>
