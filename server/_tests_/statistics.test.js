075b19f5cd80e69c4dd7b60a913a6acefb190b26
Michael Byalsky <michael.byalsky@gmail.com>
2020-10-07 15:24:20 +0300

    first test and entry point

--- server/_tests_/statistics.test.js
+++ server/_tests_/statistics.test.js
@@ -5,20 +5,32 @@ const app = require("../app");
 require("mysql2/node_modules/iconv-lite").encodingExists("foo");
 
 const { Submission, Challenge } = require("../models");
+const challenges = require("./mocks/challenges");
+const submissions = require("./mocks/submissions");
 
 //mock data
 
-describe("Cities api tests", () => {
+describe("get the all insights", () => {
     beforeAll(async () => {
       console.log("process.env.NODE_ENV", process.env.NODE_ENV);
-      
+      await Challenge.destroy({ truncate: true, force: true });
+      await Submission.destroy({ truncate: true, force: true });
+      const challengesRes = await Challenge.bulkCreate(challenges);
+      expect(challengesRes.length).toBe(3);
+      const submissionsRes = await Submission.bulkCreate(submissions);
+      expect(submissionsRes.length).toBe(6);
     });
   
     afterAll(async () => {
       app.close();
     });
   
-
-
+    it("can get all top-challenges by theirs submitting amount", async () => {
+      const { body } = await request(app).get("/api/v1/statistics/insights/top-challenges").expect(200);
+      expect(body.length).toBe(3)
+      expect(body[0].Challenge.name).toBe(challenges[0].name)
+      expect(body[1].Challenge.name).toBe(challenges[1].name)
+      expect(body[2].Challenge.name).toBe(challenges[2].name)
+    })
   });
   
--- server/api/v1/statisticsRoutes/insights.js
+++ server/api/v1/statisticsRoutes/insights.js
@@ -1,22 +1,24 @@
 const { Router } = require("express");
+const sequelize = require('sequelize')
 const router = Router();
-const { Submission, Challenge, sequelize } = require("../../../models");
-//
+const { Submission, Challenge } = require("../../../models");
 
 router.get("/top-challenges", async (req, res) => {
-  const sub = await Submission.findAll({
-    attributes: {
-      include: [
-        [sequelize.fn("COUNT", sequelize.col("challenge_id")), "n_sub"],
-      ],
-    },
-    include: {
-      model: Challenge,
-      attributes: ["name"],
-    },
-    order: [sequelize.fn("COUNT", sequelize.col("challenge_id"))],
-    limit: 10,
-  });
+    const sub = await Submission.findAll({
+      attributes: {
+        include: [
+          [sequelize.fn("COUNT", sequelize.col("challenge_id")), "count_sub"],
+        ],
+      },
+      include: {
+        model: Challenge,
+        attributes: ["name"],
+      },
+      group: ['challenge_id'],
+      order: [[sequelize.fn("COUNT", sequelize.col("challenge_id")), 'DESC']],
+      limit: 10,
+    });
+  
   res.json(sub);
 });
 
