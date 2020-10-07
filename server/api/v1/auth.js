8080f97f9c17a4b762cec543c0397bbe38c8d702
Matan Greenvlad <mgk2798@gmail.com>
2020-10-07 19:30:37 +0300

    Update auth.js

--- server/api/v1/auth.js
+++ server/api/v1/auth.js
@@ -7,27 +7,21 @@ const jwt = require("jsonwebtoken");
 ACCESS_TOKEN_SECRET = "MATAN";
 REFRESH_TOKEN_SECRET = "ITAI";
 
+// register request
 usersRouter.post("/register", async (req, res) => {
+  // if user name already exist return error
   const checkUser = await User.findOne({
     where: {
       userName: req.body.userName,
     },
   });
-  if (checkUser) {
-    res.status(409).send("user name already exists");
-  } else {
-    const hashPassword = await bcrypt.hash(req.body.password, 10);
-
-    req.body.password = hashPassword;
-
-    await User.create(req.body);
-    res.status(201).json({ message: "Register Success" });
-  }
+  if (checkUser) return res.status(409).send("user name already exists");
+  const hashPassword = await bcrypt.hash(req.body.password, 10);
+  req.body.password = hashPassword;
+  // create new user
+  await User.create(req.body);
+  res.status(201).json({ message: "Register Success" });
 });
-
-function generateToken(user) {
-  return jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: "40s" });
-}
 
 usersRouter.post("/login", async (req, res) => {
   const currentUser = await User.findOne({
@@ -43,7 +37,6 @@ usersRouter.post("/login", async (req, res) => {
   );
   if (!validPass)
     return res.status(403).json({ message: "User or Password incorrect" });
-  console.log(currentUser.dataValues);
   const refreshToken = jwt.sign(currentUser.dataValues, REFRESH_TOKEN_SECRET);
   const accessToken = generateToken(currentUser.dataValues);
   await RefreshToken.create({
@@ -74,21 +67,30 @@ usersRouter.post("/token", async (req, res) => {
     console.log(decoded);
     delete decoded.iat;
     const accessToken = generateToken(decoded);
-    res.json(accessToken);
+    res.json({ token: accessToken });
   });
 });
 
+// logout request
 usersRouter.post("/logout", async (req, res) => {
   if (!req.body.token)
     return res.status(400).json({ message: "Refresh Token Required" });
+  // check if token exist and delete it
   const result = await RefreshToken.destroy({
     where: {
       token: req.body.token,
     },
   });
+
   if (!result)
     return res.status(400).json({ message: "Refresh Token is required" });
   res.status(200).json({ message: "User Logged Out Successfully" });
+});
+
+// validate token
+usersRouter.post("/info", checkToken, (req, res) => {
+  console.log(req.decoded);
+  res.json({ message: "success get sensitive info" });
 });
 
 function checkToken(req, res, next) {
@@ -102,9 +104,8 @@ function checkToken(req, res, next) {
   });
 }
 
-usersRouter.post("/info", checkToken, (req, res) => {
-  console.log(req.decoded);
-  res.json({ message: "success get sensitive info" });
-});
+function generateToken(user) {
+  return jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+}
 
 module.exports = usersRouter;
