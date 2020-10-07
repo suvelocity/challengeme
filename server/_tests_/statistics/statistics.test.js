dedd3034c55318537e1766ec33ce7b001ba27656
Michael Byalsky <michael.byalsky@gmail.com>
2020-10-07 18:18:22 +0300

    users insights

--- server/_tests_/statistics/statistics.test.js
+++ server/_tests_/statistics/statistics.test.js
@@ -1,17 +1,17 @@
 const request = require("supertest");
-const app = require("../app");
+const app = require("../../app");
 
 //model for bulkCreate
 require("mysql2/node_modules/iconv-lite").encodingExists("foo");
 
-const { Submission, Challenge, User } = require("../models");
+const { Submission, Challenge, User } = require("../../models");
 const challenges = require("./mocks/challenges");
 const submissions = require("./mocks/submissions");
 const users = require("./mocks/users");
 
 //mock data
 
-describe("get the all insights", () => {
+describe("insights tests", () => {
     beforeAll(async () => {
       console.log("process.env.NODE_ENV", process.env.NODE_ENV);
       await Challenge.destroy({ truncate: true, force: true });
@@ -22,7 +22,6 @@ describe("get the all insights", () => {
       const submissionsRes = await Submission.bulkCreate(submissions);
       expect(submissionsRes.length).toBe(6);
       const userRes = await User.bulkCreate(users);
-      console.log(userRes);
       expect(userRes.length).toBe(3);
     });
   
@@ -30,6 +29,14 @@ describe("get the all insights", () => {
       app.close();
     });
   
+    it("can get all top-challenges by theirs submitting amount", async () => {
+      const { body } = await request(app).get("/api/v1/statistics/insights/top-challenges").expect(200);
+      expect(body.length).toBe(3)
+      expect(body[0].Challenge.name).toBe(challenges[0].name)
+      expect(body[1].Challenge.name).toBe(challenges[1].name)
+      expect(body[2].Challenge.name).toBe(challenges[2].name)
+    })
+
     it("can get all top-challenges by theirs submitting amount", async () => {
       const { body } = await request(app).get("/api/v1/statistics/insights/top-challenges").expect(200);
       expect(body.length).toBe(3)
