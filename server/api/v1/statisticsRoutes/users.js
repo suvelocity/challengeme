14bc22e1c3b75a20361ecc9ecf04e72bbf23fddc
RoyShnitzel <67857836+RoyShnitzel@users.noreply.github.com>
2020-10-07 17:51:22 +0300

    top users endpoint

--- server/api/v1/statisticsRoutes/users.js
+++ server/api/v1/statisticsRoutes/users.js
@@ -1,10 +1,22 @@
-const { Router } = require('express');
-const router = Router()
-const { Submission, Challenge } = require('../../../models');
+const { Router } = require("express");
+const router = Router();
+const sequelize = require("sequelize");
+const { Submission, Challenge, User } = require("../../../models");
 
-
-router.get('/top-users', async (req, res) => {
-    res.send('hello world users')
-  })
+router.get("/top-users", async (req, res) => {
+  const topUsers = await Submission.findAll({
+    attributes: {
+      include: [[sequelize.fn("COUNT", sequelize.col("user_id")), "countUser"]],
+    },
+    include: {
+      model: User,
+      attributes: ["userName"],
+    },
+    group: ["user_id"],
+    order: [[sequelize.fn("COUNT", sequelize.col("user_id")), "DESC"]],
+    limit: 10,
+  });
+  res.json(topUsers);
+});
 
 module.exports = router;
