60bc884ab0e306794af18828122dd6ae7ba8cb8b
ShaharEli <shahar.e3@gmail.com>
2020-10-07 11:51:59 +0300

    Update AddImg.js

--- client/src/components/AddImg.js
+++ client/src/components/AddImg.js
@@ -1,12 +1,22 @@
-import React,{useMemo, useState} from 'react'
+import React,{ useState} from 'react'
 import "./AddImg.css"
 import DropNCrop from '@synapsestudios/react-drop-n-crop';
 
 import '@synapsestudios/react-drop-n-crop/lib/react-drop-n-crop.min.css';
 import { Button } from '@material-ui/core';
 function AddImg() {
-const [file,setFile] = useState("")
+const [file,setFile] = useState({})
+const [blobedImg,setBlobedImg] = useState(null)
 const onChange = value => {
+    // const reader = new FileReader();
+    // let pizza = reader.readAsDataURL(value.src); 
+    // console.log(pizza);
+    console.log(value.src);
+    
+    fetch(value.src)
+    .then(res => res.blob())
+    .then(res=>setBlobedImg(res))
+
     setFile(value);
   };
 
@@ -15,16 +25,25 @@ const onChange = value => {
   
 
     return (
+        <>
         <div className="upload-img-container">
             <div className="upload-img-secondary-container">
-            <DropNCrop cropperOptions={{aspectRatio:400/150,height:200}} onChange={onChange} value={file} />
-        <Button variant="contained" color="secondary" contained onClick={()=>setFile("")} >
+            <DropNCrop maxFileSize={5145728} cropperOptions={{multiple:false,minCropBoxHeight:300,minCropBoxWidth:800,aspectRatio:400/150}} onChange={onChange} value={file} />
+        <Button variant="contained" color="secondary" onClick={()=>setFile({})} >
             Remove img
         </Button>
             </div>
 
         </div>
+        {
+            blobedImg
+            &&
+            <img src={URL.createObjectURL(blobedImg)} />
+            
+        }
+        </>
     )
+
 }
 
 export default AddImg
