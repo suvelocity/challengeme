2f9f6b13c0f1139393a53784f11efb0c5dbbccac
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-10-08 13:43:22 +0300

    setting up to handle the webhook

--- server/api/v1/submissions.js
+++ server/api/v1/submissions.js
@@ -1,5 +1,16 @@
 const { Router } = require('express');
+const { Submission } = require('../../models');
 const router = Router();
+
+/* {
+    solutionRepo: 'TheAlmightyCrumb/calculator-challenge-Submission7', // first part Solution Repository after it Submission Id
+    success: false // true or false
+} */
+// getting id from the SolutionRepositry
+/* const string1 = 'Submission';
+const index = data.jobs[0].name.lastIndexOf(string1);
+const stringLength = string1.length;//index === -1 ? string2.length : string1.length;
+const submissionId = data.jobs[0].name.slice(index+stringLength); */
 
 router.post('/', (req, res) => {
     console.error(req.body);
--- server/index.js
+++ server/index.js
@@ -5,19 +5,20 @@ const checkActions = require('./lib/check-actions');
 const port = process.env.PORT || 8080
 
 async function establishConnection(){
-  const url = await ngrok.connect(port);
   //const apiUrl = ngrok.getUrl();
   console.log(process.env.NODE_ENV)
-  if(process.env.NODE_ENV == 'development'){
+  if(process.env.NODE_ENV === 'development'){
+    const url = await ngrok.connect(port);
     process.env.MY_URL = url; 
   }
   console.log(process.env.MY_URL);
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
+  setInterval(checkActions, 10000) // todo: Make Submissions Update without Me
 }
 establishConnection();  // node env development
-setInterval(checkActions, 10000)
+
 
 
 
