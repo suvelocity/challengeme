d4d0b64372b68ea751315ba3dcdeb1ff52cca406
AmitNirGit <67857460+AmitNirGit@users.noreply.github.com>
2020-10-08 21:15:24 +0300

    not-broken-dev

--- client/src/components/ChallengePage/ChallengePage.js
+++ client/src/components/ChallengePage/ChallengePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {useParams} from 'react-router-dom';
+import { useParams } from "react-router-dom";
 // import axios from 'axios'
 import network from "../../services/network";
 
@@ -7,10 +7,11 @@ import { Button, Link } from "@material-ui/core";
 import Rating from "@material-ui/lab/Rating";
 import Chip from "@material-ui/core/Chip";
 
-import ChallengeTabs from "./Tabs";
+// import ChallengeTabs from "./SolutionTable";
 //import SubmitModal from "./SubmitModal";
 
 import SubmitModal from "./SubmitModal";
+import SolutionTable from "./SolutionTable";
 
 // import ChallengePage from "./ChallengePage";
 //import SubmissionTable from "./SubmissionTable";
@@ -18,202 +19,160 @@ import SubmitModal from "./SubmitModal";
 import "./ChallengePage.css";
 
 const challenge = {
-	cover: "https://storage.googleapis.com/challenges-cover/tvv_f.png",
-	createdAt: "2020-10-04T12:00:00.000Z",
-	deletedAt: null,
-	description: "in this challenge we up cloneup clone this and start hirluling",
-	githubLink: "https://github.com/suvelocity/TV-shows-boilerplate",
-	id: 4,
-	name: "React - Tv shows",
-	repositoryName: "suvelocity/TV-shows-challenge",
-	type: "client",
-	updatedAt: "2020-10-04T12:00:00.000Z",
-	createdBy: "user231",
-	label: ["React", "JS", "Promise"],
-	rating: 3.7,
-	isSaved: true
+  cover: "https://storage.googleapis.com/challenges-cover/tvv_f.png",
+  createdAt: "2020-10-04T12:00:00.000Z",
+  deletedAt: null,
+  description: "in this challenge we up cloneup clone this and start hirluling",
+  githubLink: "https://github.com/suvelocity/TV-shows-boilerplate",
+  id: 4,
+  name: "React - Tv shows",
+  repositoryName: "suvelocity/TV-shows-challenge",
+  type: "client",
+  updatedAt: "2020-10-04T12:00:00.000Z",
+  createdBy: "user231",
+  label: ["React", "JS", "Promise"],
+  rating: 3.7,
+  isSaved: true,
 };
 
 const challengeParamId = 3; //Mock until we merge shahar
 const userId = 2; //Mock until we merge shahar
 
-const makeBlobed = async img => {
-	const preBlobedImg = await fetch(img);
-	const blobedImg = await preBlobedImg.blob();
-	return blobedImg;
-};
-
 function ChallengePage() {
-
   // const [challenge, setChallenge] = useState({});
-  const params = useParams();
+  const { challengeId } = useParams();
 
-	//const [challengeInfo,setChallengeInfo] = useState('');
-	const [blobedImg, setBlobedImg] = useState("");
-	const [isModalOpen, setIsModalOpen] = useState(false);
+  //const [challengeInfo,setChallengeInfo] = useState('');
+  const [blobedImg, setBlobedImg] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-	const setImg = async () => {
-		const { data } = await network.get(`/api/v1/images?id=${1}`);
-		console.log(data);
-		const blobed = await makeBlobed(data.img);
-		const imgURL = URL.createObjectURL(blobed);
-		// console.log(imgURL);
-		setBlobedImg(imgURL);
-	};
-	useEffect(() => {
-		//setChallengeInfo(axios.get('url:id').data);
-		// console.log(blobedImg);
-		setImg();
-		console.log(blobedImg);
-		// =======
-		// const [challenge, setChallenge] = useState({});
+  const setImg = async () => {
+    const { data } = await network.get(`/api/v1/images?id=${challengeId}`);
+    setBlobedImg(data.img);
+  };
+  useEffect(() => {
+    //setChallengeInfo(axios.get('url:id').data);
+    // console.log(blobedImg);
+    setImg();
+    console.log(blobedImg);
+    // =======
+    // const [challenge, setChallenge] = useState({});
 
-		// useEffect(() => {
-		//   const fetchChallenge = async () => {
-		//     try {
-		//       const { data: challengeFromServer } = await axios.get(
-		//         `/api/v1/challenges/${challengeParamId}`
-		//       );
-		//       console.table(challenge);
-		//       // setChallenge(challengeFromServer);
-		//     } catch (error) {
-		//       console.log(error);
-		//     }
-		//   };
-		//   fetchChallenge();
-		// >>>>>>> 36f2077c2c283d389024cb3a91620c0a4000d5e0
-	}, []);
+    // useEffect(() => {
+    //   const fetchChallenge = async () => {
+    //     try {
+    //       const { data: challengeFromServer } = await axios.get(
+    //         `/api/v1/challenges/${challengeParamId}`
+    //       );
+    //       console.table(challenge);
+    //       // setChallenge(challengeFromServer);
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   };
+    //   fetchChallenge();
+    // >>>>>>> 36f2077c2c283d389024cb3a91620c0a4000d5e0
+  }, []);
 
+  function handleModalClose() {
+    setIsModalOpen(false);
+  }
 
-	function handleModalClose() {
-		setIsModalOpen(false);
-	}
-
-	return challenge ? (
-		<div className="fullpage-wrapper">
-			<div className="navbar">im navbar</div>
-			<div className="challenge-wrapper">
-				<div className="challenge-left-wrapper">
-					<div className="challenge-img-div">
-						<img className="challenge-img" src={challenge.cover} />
-					</div>
-					<div className="challenge-rawdata">
-						<span className="challenge-created-by">
-							<p> created by: </p> <p>user name</p>
-						</span>
-						<span className="challenge-created-at">
-							<p> Created at: </p>{" "}
-							<p>{normalizeDate(challenge.createdAt) + " "} </p>
-						</span>
-						<span className="challenge-updated-at">
-							<p> Updated at: </p> <p>{normalizeDate(challenge.updatedAt)}</p>
-						</span>
-					</div>
-					<div className="challenge-labels">
-						<h2>Labels:</h2>
-						<span className="challenge-label">
-							<Chip
-								color="secondary"
-								label="difficulty"
-								component="a"
-								href="#chip"
-								clickable
-							/>
-						</span>
-						{challenge.label.map((tag, index) => (
-							<span key={index} className="challenge-label">
-								<Chip
-									color="primary"
-									label={tag}
-									component="a"
-									href="#chip"
-									clickable
-								/>
-							</span>
-						))}
-					</div>
-					<div className="challenge-rating">
-						<h2>Rating:</h2>
-						<Rating
-							name="half-rating-read"
-							defaultValue={3}
-							precision={0.5}
-							readOnly
-							size="large"
-						/>
-					</div>
-					<div className="challenge-github-btn">
-						<Button color="primary" href={challenge.githubLink}>
-							To Github!
-						</Button>
-					</div>
-				</div>
-
-        <Button color="primary" href={challenge.githubLink}>
-          To Github!
-        </Button>
-        <div className="challenge-rating">
-          <Rating
-            name="half-rating-read"
-            defaultValue={3}
-            precision={0.5}
-            readOnly
-          />
-        </div>
-        <span className="challenge-difficulty"></span>
-      </div>
-      <div className="challenge-description-wrapper">
-        <div className="challenge-rawdata">
-          <span className="challenge-created-at">
-            Created at: {normalizeDate(challenge.createdAt) + " "}
-          </span>
-          <span className="challenge-updated-at">
-            Updated at: {normalizeDate(challenge.updatedAt)}
-          </span>
-        </div>
-        <div className="challenge-description">
-          {challenge.label.map((tag, index) => (
-            <span key={index} className="challenge-label">
+  return challenge ? (
+    <div className='fullpage-wrapper'>
+      <div className='navbar'>im navbar</div>
+      <div className='challenge-wrapper'>
+        <div className='challenge-left-wrapper'>
+          <div className='challenge-img-div'>
+            <img className='challenge-img' src={blobedImg} />
+          </div>
+          <div className='challenge-rawdata'>
+            <span className='challenge-created-by'>
+              <p> created by: </p> <p>user name</p>
+            </span>
+            <span className='challenge-created-at'>
+              <p> Created at: </p>{" "}
+              <p>{normalizeDate(challenge.createdAt) + " "} </p>
+            </span>
+            <span className='challenge-updated-at'>
+              <p> Updated at: </p> <p>{normalizeDate(challenge.updatedAt)}</p>
+            </span>
+          </div>
+          <div className='challenge-labels'>
+            <h2>Labels:</h2>
+            <span className='challenge-label'>
               <Chip
-                color="primary"
-                label={tag}
-                component="a"
-                href="#chip"
+                color='secondary'
+                label='difficulty'
+                component='a'
+                href='#chip'
                 clickable
               />
             </span>
-          ))}
-          <p className="challenge-description">{challenge.description}</p>
-          {/* change prop to params.id */}
-          <ChallengeTabs challengeId = {challengeParamId}/>
-          <Button
-							color="primary"
-							className="submit-btn"
-							onClick={setIsModalOpen}>
-							Submit
-						</Button>
+            {challenge.label.map((tag, index) => (
+              <span key={index} className='challenge-label'>
+                <Chip
+                  color='primary'
+                  label={tag}
+                  component='a'
+                  href='#chip'
+                  clickable
+                />
+              </span>
+            ))}
+          </div>
+          <div className='challenge-rating'>
+            <h2>Rating:</h2>
+            <Rating
+              name='half-rating-read'
+              defaultValue={3}
+              precision={0.5}
+              readOnly
+              size='large'
+            />
+          </div>
+          <div className='challenge-github-btn'>
+            <Button color='primary' href={challenge.githubLink}>
+              To Github!
+            </Button>
+          </div>
+        </div>
+        <div className='challenge-right-wrapper'>
+          <div className='challenge-title-description'>
+            <div className='challenge-name'>
+              <h1>{challenge.name}</h1>
+            </div>
+            <div className='challenge-description'>
+              <p>{challenge.description}</p>
+            </div>
+            {/* change prop to params.id */}
+          </div>
+          <div className='challenge-solution-table'>
+            <SolutionTable challengeId={challengeParamId} />
+          </div>
+          <div className='challenge-submit-btn'>
+            <Button color='primary' onClick={setIsModalOpen}>
+              Submit
+            </Button>
+          </div>
         </div>
       </div>
 
-
-				<SubmitModal
-					isOpen={isModalOpen}
-					handleClose={handleModalClose}
-					challengeId={challengeParamId}
-					userId={userId}
-				/>
-				{/* <SubmissionTable /> */}
-				{/* <ReviewsSection /> */}
-			</div>
-		</div>
-	) : (
-		<div>Loading</div>
-	);
+      <SubmitModal
+        isOpen={isModalOpen}
+        handleClose={handleModalClose}
+        challengeId={challengeParamId}
+        userId={userId}
+      />
+    </div>
+  ) : (
+    <div>Loading</div>
+  );
 }
 
 function normalizeDate(dateTime) {
-	//"2020-10-04T12:00:00.000Z";
-	const date = dateTime.split("T")[0];
-	return date;
+  //"2020-10-04T12:00:00.000Z";
+  const date = dateTime.split("T")[0];
+  return date;
 }
 export default ChallengePage;
---
+++ client/src/components/ChallengePage/SolutionTable.js
@@ -0,0 +1,103 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import PhoneIcon from "@material-ui/icons/Phone";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
+import HelpIcon from "@material-ui/icons/Help";
+import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
+import ThumbDown from "@material-ui/icons/ThumbDown";
+import ThumbUp from "@material-ui/icons/ThumbUp";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import SubmissionTable from "./SubmissionTable";
+import ReviewSection from "./ReviewSection";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role='tabpanel'
+      hidden={value !== index}
+      id={`scrollable-force-tabpanel-${index}`}
+      aria-labelledby={`scrollable-force-tab-${index}`}
+      {...other}>
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-force-tab-${index}`,
+    "aria-controls": `scrollable-force-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function ScrollableTabsButtonForce(props) {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div
+      className={classes.root}
+      style={{
+        width: "725px",
+        height: "300px",
+        backgroundColor: "lightskyblue",
+        overflowY: "auto",
+      }}>
+      <AppBar position='static' color='default'>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant='scrollable'
+          scrollButtons='on'
+          indicatorColor='primary'
+          textColor='primary'
+          aria-label='scrollable force tabs example'>
+          <Tab label='Submissions' {...a11yProps(0)} />
+          <Tab label='Reviews' {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel
+        style={{ backgroundColor: "lightskyblue" }}
+        value={value}
+        index={0}>
+        <SubmissionTable challengeId={props.challengeId} />
+      </TabPanel>
+      <TabPanel
+        style={{ backgroundColor: "lightskyblue" }}
+        value={value}
+        index={1}>
+        <ReviewSection challengeId={props.challengeId} />
+      </TabPanel>
+    </div>
+  );
+}
