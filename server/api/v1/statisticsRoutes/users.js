0f7e7d62709e46814753b1a7c399da401baaa294
MrMoshkovitz <gal@gmail.com>
2020-10-08 19:10:21 +0300

    Statistics Done

--- server/api/v1/statisticsRoutes/users.js
+++ server/api/v1/statisticsRoutes/users.js
@@ -1,19 +1,10 @@
 
 
-// SELECT users.id, submissions.state, submissions.created_at, COUNT(submissions.id)
-// FROM `submissions` 
-// JOIN users
-// ON users.id = submissions.user_id
-// WHERE user_id=7 AND
-// DATE(submissions.created_at) > (NOW() -INTERVAL 7 DAY)
-// GROUP BY submissions.state
-
-
 const { Router } = require("express");
 const router = Router();
 const sequelize = require("sequelize");
 const { Submission, Challenge, User } = require("../../../models");
-const { Op } = require("sequelize");
+const { Op, where } = require("sequelize");
 
 router.get("/top-users", async (req, res) => {
   const topUsers = await Submission.findAll({
@@ -24,6 +15,7 @@ router.get("/top-users", async (req, res) => {
       model: User,
       attributes: ["userName"],
     },
+    where: {state: "SUCCESS"},
     group: ["user_id"],
     order: [[sequelize.fn("COUNT", sequelize.col("user_id")), "DESC"]],
     limit: 10,
@@ -56,8 +48,6 @@ router.get("/user-success", async (req, res) => {
   res.json(subBySuccess);
 });
 
-
-
 router.get("/sub-by-date", async (req, res) => {
   let loggedUser = req.user.userId ? req.user.userId : 7
   const subByDate = await Submission.findAll({
@@ -79,7 +69,7 @@ router.get("/sub-by-date", async (req, res) => {
   res.json([subByDate, req.user]);
 });
 
-router.get("/stronger-side", async(req, res) => {
+router.get("/sub-by-type", async(req, res) => {
   let loggedUser = req.user ? req.user.userId : 1
   const subByType = await Submission.findAll({
     include: [
@@ -103,18 +93,31 @@ router.get("/stronger-side", async(req, res) => {
   res.json(subByType)
 })
 
-// attributes: [
-//   "id",
-//   "state",
-//   "createdAt",
-//   [sequelize.fn("COUNT", sequelize.col("user_id")), "CountByUserID"]
-// ],
+router.get("/unsolved-challenges", async(req, res) => {
+  let loggedUser = req.user ? req.user.userId : 1
+  const userSubmissions = await Submission.findAll({
+    group:["challenge_id"],
+    attributes: [
+      "challenge_id"
+    ],
+    where: {
+      userId: loggedUser
+    }
+  })
 
-// where: {
-//   [Op.and]: [
-//     {
-//       userId: loggedUser
-//     }, {[Op.or]: [{ state: "SUCCESS" }, { state: "FAIL" }]}
-//   ],
+  const solvedChallenges = userSubmissions.map(challenge => {
+    return challenge.challenge_id
+  })
+
+
+  const unsolvedChallenges = await Challenge.findAll({
+      where: {
+      id: {[Op.notIn]: solvedChallenges}
+  }})
+
+  res.json(unsolvedChallenges)
+})
+
+
   
 module.exports = router;
