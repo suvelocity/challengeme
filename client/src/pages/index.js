144f17456008baed5073514782b82f3a0f23b6c4
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-10-08 09:50:35 +0300

    added logged to router

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter,
   Switch,
@@ -8,13 +8,25 @@ import Home from './Home';
 import Register from './Register/Register';
 
 export default function Router() {
+  const [logged, setLogged] = useState(false);
+
   return (
     <BrowserRouter>
+      {!logged ? 
       <Switch>
-        <Route path="/">
+        <Route path="/register">
           <Register />
         </Route>
+        {/* <Route path="/login">
+          <Register />
+        </Route> */}
+      </Switch> :
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
       </Switch>
+    }
     </BrowserRouter>
   );
 }
