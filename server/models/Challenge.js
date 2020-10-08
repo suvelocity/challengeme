73dc23c781c0cb3f37fcc416ee73e8281f73624b
tsach ovadia <67857438+ItsDrTsach@users.noreply.github.com>
2020-10-08 09:11:27 +0300

    refactor some code

--- server/models/Challenge.js
+++ server/models/Challenge.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Challenge extends Model {
     /**
@@ -9,18 +7,23 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-  };
-  Challenge.init({
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    type: DataTypes.STRING,
-    repositoryName: DataTypes.STRING,
-    cover: DataTypes.STRING,
-  }, {
-    sequelize,
-    paranoid: true,
-    modelName: 'Challenge',
-    tableName: 'challenges'
-  });
+  }
+  Challenge.init(
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      type: DataTypes.STRING,
+      repositoryName: DataTypes.STRING,
+      cover: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      paranoid: true,
+      modelName: "Challenge",
+      tableName: "challenges",
+    }
+  );
   return Challenge;
 };
+
+// TODO: add the clolumn githubLink(STRING), userId(integer)
