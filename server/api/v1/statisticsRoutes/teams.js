caab84b17cd3352fe6df6f56109e162cbd989a96
trodelgal <67858041+trodelgal@users.noreply.github.com>
2020-10-08 19:32:31 +0300

    top and users submissions by group

--- server/api/v1/statisticsRoutes/teams.js
+++ server/api/v1/statisticsRoutes/teams.js
@@ -2,19 +2,22 @@ const { Router } = require('express');
 const sequelize = require("sequelize");
 const { Op } = require("sequelize");
 const router = Router()
-const { Submission, Challenge, Teams, User } = require('../../../models');
+const { Submission, Teams, User, UsersTeams } = require('../../../models');
 
 
 router.get('/top', async (req, res) => {
+  try{
     const topTeam = await Teams.findAll({
+      raw:true,
       group: ["id"],
       attributes: ['id','name'],
       include:[
         {
           model:User,
+          attributes:[],
           include:{
             model: Submission,
-            attributes:[[sequelize.fn("COUNT", sequelize.col("id")), "teamSubmissions"]],
+            attributes:[[sequelize.fn("COUNT", sequelize.col("challenge_id")), "teamSuccessSubmissions"]],
             where:{
               state:'success'
             }
@@ -22,21 +25,98 @@ router.get('/top', async (req, res) => {
         }
       ]
     })
-    if(err){
-      res.send(err)
-    }
+    topTeam.forEach(element => {
+      delete element["Users.UsersTeams.teamId"]
+      delete element["Users.UsersTeams.userId"]
+      delete element["Users.UsersTeams.createdAt"]
+      delete element["Users.UsersTeams.updatedAt"]
+      delete element["Users.UsersTeams.createdAt"]
+      delete element["Users.UsersTeams.TeamId"]
+      delete element["Users.Submissions.id"]
+      delete element["Users.UsersTeams.UserId"]
+      delete element["Users.UsersTeams.createdAt"]
+    })
     res.send(topTeam)
+  }catch(err){
+    res.status(400).send(err)
+  }
   })
 
-module.exports = router;
+  router.get('/top-user', async (req, res) => {
+    try{
+      const teams = await Teams.findByPk(1)
+      const users = await teams.getUsers({
+        raw:true,
+        group:["id"],
+        attributes:["id", "firstName", "lastName"],
+        include:[
+          {
+          model:Submission,
+          attributes:[[sequelize.fn("COUNT", sequelize.col("challenge_id")), "userSuccessSubmission"]],
+          where:{
+            state:'success'
+          }
+          }
+        ]
+      })
+      users.forEach(element => {
+        delete element["UsersTeams.teamId"]
+        delete element["UsersTeams.userId"]
+        delete element["UsersTeams.createdAt"]
+        delete element["UsersTeams.updatedAt"]
+        delete element["UsersTeams.TeamId"]
+        delete element["UsersTeams.UserId"]
+      })
+      res.send(users)
+    }catch(err){
+      res.status(400).send(err)
+    }
+    })
 
-// select teams.name, count(submissions.id) 
-// from teams
-// join users_teams
-// on teams.id = users_teams.team_id
-// join users
-// on users_teams.user_id = users.id
-// join submissions
-// on users.id = submissions.user_id
-// where submissions.state = 'SUCCESS'
-// group by teams.id
+    router.get('/last-week-submissions', async (req, res) => {
+      try{
+        const team = await Teams.findAll({
+          raw:true,
+          group:[sequelize.fn("DAY", "submissions.createdAt")],
+          attributes: ['id','name',[sequelize.fn("COUNT", "submissions.id"), "teamSubmissions"]],
+          where:{
+            id: 1
+          },
+          include:[
+            {
+              model:User,
+              attributes:[],
+              include:{
+                model: Submission,
+                attributes:["createdAt"],
+                where:{
+                  state:'SUCCESS',
+                  created_at: {
+                    [Op.gte]: new Date(Date.now() - 604800000),
+                  }
+                }
+              }
+            }
+          ]
+        })
+
+        const taem = await Submission.findAll({
+          group:[sequelize.fn("DAY", sequelize.col("created_at"))],
+          attributes: ['id',[sequelize.fn("COUNT", "id"), "teamSubmissions"]],
+        })
+        team.forEach(element => {
+          delete element["Users.UsersTeams.teamId"]
+          delete element["Users.UsersTeams.userId"]
+          delete element["Users.UsersTeams.createdAt"]
+          delete element["Users.UsersTeams.updatedAt"]
+          delete element["Users.UsersTeams.TeamId"]
+          delete element["Users.Submissions.id"]
+          delete element["Users.UsersTeams.UserId"]
+        })
+        res.send(taem)
+      }catch(err){
+        res.status(400).send(err)
+      }
+      })
+
+module.exports = router;
