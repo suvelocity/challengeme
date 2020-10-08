c44f2f19f5eb85f738ba0c4b8b2412420cc228bf
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-10-08 15:30:40 +0300

    put instead of push

--- server/api/v1/webhook.js
+++ server/api/v1/webhook.js
@@ -2,31 +2,21 @@ const { Router } = require('express');
 const { Submission } = require('../../models');
 const router = Router();
 
-
 /* {
     solutionRepo: 'TheAlmightyCrumb/calculator-challenge-Submission7', // first part Solution Repository after it Submission Id
     success: false // true or false
 } */
+
 // getting id from the SolutionRepositry
 /* const string1 = 'Submission';
 const index = data.jobs[0].name.lastIndexOf(string1);
 const stringLength = string1.length;//index === -1 ? string2.length : string1.length;
 const submissionId = data.jobs[0].name.slice(index+stringLength); */
 
-router.post('/', async (req, res) => {
-const { solutionRepo , success} = req.body;
-console.log(solutionRepo.split('-Submission')[0])
-  let submission = await Submission.findOne({
-    where: {
-      solutionRepository: solutionRepo.split('-Submission')[0]
-    }
-  });
-  await submission.update({ state: success ? 'SUCCESS' : 'FAIL' })
-    res.json(submission);
+router.post('/submission/:id', (req, res) => {
+    console.error(req.params.id);
+    console.error(req.body);
+    res.json('success');
 })
-
-
-
-
 
 module.exports = router;
