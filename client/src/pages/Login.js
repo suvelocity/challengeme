0a944b150033ae1219774becae90a9f677f831d7
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-10-08 14:07:28 +0300

    login design

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,110 +3,225 @@ import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Cookies from "js-cookie";
 import axios from "../services/network";
-import { Logged } from '../context/LoggedInContext';
+import { Logged } from "../context/LoggedInContext";
+import IconButton from "@material-ui/core/IconButton";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import FormControl from "@material-ui/core/FormControl";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import PeopleIcon from "@material-ui/icons/People";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import LockIcon from "@material-ui/icons/Lock";
+import Button from "@material-ui/core/Button";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import ErrorIcon from "@material-ui/icons/Error";
+
+import "../styles/Login.css";
+
+const useStyles = makeStyles((theme) => ({
+    userName: {
+        marginTop: "80px",
+        marginBottom: "20px",
+        width: "320px",
+    },
+    password: {
+        marginBottom: "5px",
+        width: "320px",
+    },
+    loginButton: {
+        marginBottom: "10px",
+        marginTop: "60px",
+        background: "linear-gradient(45deg, #447CC6 30%, #315CAB 90%)",
+        color: "white",
+    },
+}));
 
 export default function Login() {
-  const [userName, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [rememberMe, setRememberMe] = useState(false);
-  const [error, setError] = useState({});
+    const classes = useStyles();
+    const [userName, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [rememberMe, setRememberMe] = useState(false);
+    const [error, setError] = useState({});
+    const [showPassword, setShowPassword] = useState(false);
+    const location = useHistory();
 
-  const location = useHistory()
+    const value = useContext(Logged);
 
-  const value = useContext(Logged);
-  
-  const updateField = (e) => {
-    switch (e.currentTarget.name) {
-      case "password":
-        setPassword(e.currentTarget.value);
-        break;
-      case "userName":
-        setUsername(e.currentTarget.value);
-        break;
-      case "rememberMe":
-        setRememberMe((prevState) => !prevState);
-        break;
-    }
-  };
+    const updateField = (e) => {
+        switch (e.currentTarget.name) {
+            case "password":
+                setPassword(e.currentTarget.value);
+                break;
+            case "userName":
+                setUsername(e.currentTarget.value);
+                break;
+            case "rememberMe":
+                setRememberMe((prevState) => !prevState);
+                break;
+        }
+    };
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+    const loginFunc = async (e) => {
+        const formErrors = {};
+        e.preventDefault();
+        if (/\W/.test(userName)) {
+            formErrors.userName = "invalid userName";
+        }
+        if (userName.length < 1 || userName.length > 32) {
+            formErrors.userName = "userName must be 1-32 characters long";
+        }
 
-  const loginFunc = async (e) => {
-    const formErrors = {};
-    e.preventDefault();
-    if (/\W/.test(userName)) {
-      formErrors.userName = "invalid userName";
-    }
-    if (userName.length < 6 || userName.length > 32) {
-      formErrors.userName = "userName must be 6-32 characters long";
-    }
+        if (password.length < 5) {
+            formErrors.password = "password must be at least 8 characters long";
+        }
+        if (formErrors.password || formErrors.userName) {
+            setError(formErrors);
+            return;
+        }
+        //request to server
+        const { data: response } = await axios.post("/api/v1/auth/login", {
+            userName: userName,
+            password: password,
+            rememberMe: rememberMe,
+        });
+        //if success -> set cookies
+        if (response.message) {
+            setError({ msg: response.message });
+            return;
+        }
+        Cookies.set("accessToken", response.accessToken);
+        Cookies.set("refreshToken", response.refreshToken);
+        value.setLogged(true);
+        location.push("/");
+    };
 
-    if (password.length < 5) {
-      formErrors.password = "password must be at least 8 characters long";
-    }
-    if (formErrors.password || formErrors.userName) {
-      setError(formErrors);
-      return;
-    }
-    //request to server
-    const { data: response } = await axios.post("/api/v1/auth/login", {
-      userName: userName,
-      password: password,
-      rememberMe: rememberMe,
-    });
-    //if success -> set cookies
-    if (response.message) {
-      setError({ msg: response.message });
-      return;
-    }
-    Cookies.set("accessToken", response.accessToken);
-    Cookies.set("refreshToken", response.refreshToken);
-    value.setLogged(true);
-    location.push('/')
-  };
-
-  return (
-    <div>
-      <button>Log in with google</button>
-      <button>Log in with github</button>
-      <form onSubmit={loginFunc}>
-        <input
-          type="text"
-          id="userName-field"
-          name="userName"
-          value={userName}
-          required
-          onChange={updateField}
-        />
-        <input
-          type="password"
-          id="password-field"
-          name="password"
-          value={password}
-          required
-          onChange={updateField}
-        />
-        <button type="submit" id="login-button">
-          Login
-        </button>
-      </form>
-      <label htmlFor="rememberMe">Remember Me: </label>
-      <input
-        type="checkbox"
-        id="rememberMe"
-        name="rememberMe"
-        onChange={updateField}
-      />
-      <br />
-      <span>don't have an account yet?</span>
-      <Link to="/register">
-        <button id="signUp">Sign up</button>
-      </Link>
-      <br />
-
-      {
-        <div style={{ backgroundColor: "red" }}>
-          {error.userName || error.password || error.msg}
+    return (
+        <div className="loginGeneral">
+            <div className="containerHeader">
+                <div className="loginHeader">
+                    <div className="loginTitle">Log in</div>
+                    <div className="orLoginWith">Or login with :</div>
+                    <div>
+                        <IconButton>
+                            <FacebookIcon style={{ color: "white" }} />
+                        </IconButton>
+                        <IconButton>
+                            <GitHubIcon style={{ color: "white" }} />
+                        </IconButton>
+                    </div>
+                </div>
+            </div>
+            <div className="containerBody">
+                <form className="loginForm" onSubmit={loginFunc}>
+                    <div className="loginBody">
+                        <FormControl className={classes.userName}>
+                            <InputLabel
+                                style={{ color: "grey" }}
+                                htmlFor="standard-adornment-password"
+                            >
+                                User Name
+                            </InputLabel>
+                            <Input
+                                type="text"
+                                id="userName-field"
+                                name="userName"
+                                // color="secondary"
+                                value={userName}
+                                required
+                                onChange={updateField}
+                                endAdornment={
+                                    <InputAdornment
+                                        style={{ opacity: "0.7" }}
+                                        position="end"
+                                    >
+                                        <PeopleIcon />
+                                    </InputAdornment>
+                                }
+                            />
+                        </FormControl>
+                        <FormControl className={classes.password}>
+                            <InputLabel
+                                style={{ color: "grey" }}
+                                className={classes.labelPass}
+                                htmlFor="standard-adornment-password"
+                            >
+                                Password
+                            </InputLabel>
+                            <Input
+                                id="password-field"
+                                name="password"
+                                value={password}
+                                required
+                                type={showPassword ? "text" : "password"}
+                                onChange={updateField}
+                                // color="secondary"
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            style={{ opacity: "0.7" }}
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                        >
+                                            {showPassword ? (
+                                                <Visibility />
+                                            ) : (
+                                                <VisibilityOff />
+                                            )}
+                                        </IconButton>
+                                        <LockIcon style={{ opacity: "0.7" }} />
+                                    </InputAdornment>
+                                }
+                            />
+                        </FormControl>
+                        <Link to="/forgotpassword" className="forgotLabel">
+                            Forgot Password ?
+                        </Link>
+                        {(error.userName || error.password || error.msg) && (
+                            <div className="containerError">
+                                <ErrorIcon
+                                    style={{
+                                        color: "white",
+                                        marginLeft: "4px",
+                                    }}
+                                />
+                                <div className="errorInput">
+                                    {error.userName ||
+                                        error.password ||
+                                        error.msg}
+                                </div>
+                            </div>
+                        )}
+                        <Button
+                            type="submit"
+                            id="login-button"
+                            className={classes.loginButton}
+                        >
+                            Log in
+                        </Button>
+                        <FormControlLabel
+                            htmlFor="rememberMe"
+                            value="start"
+                            control={<Checkbox color="primary" />}
+                            label="Remember me"
+                            labelPlacement="end"
+                            name="rememberMe"
+                            type="checkbox"
+                            onChange={updateField}
+                        />
+                        <div>
+                            <span>don't have an account yet?</span>
+                            <Link to="/register">Sign up</Link>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
-      }
-    </div>
-  );
+    );
 }
