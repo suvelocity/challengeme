85df73fc66de80c7ac6d462ad4f0380781081197
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-10-08 10:39:14 +0300

    server start after ngrok connection
    server start after ngrok connection and url is set only if environment is set to development

--- server/index.js
+++ server/index.js
@@ -7,14 +7,17 @@ const port = process.env.PORT || 8080
 async function establishConnection(){
   const url = await ngrok.connect(port);
   //const apiUrl = ngrok.getUrl();
-  process.env.MY_URL = url;  
+  console.log(process.env.NODE_ENV)
+  if(process.env.NODE_ENV == 'development'){
+    process.env.MY_URL = url; 
+  }
+  console.log(process.env.MY_URL);
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
 }
-establishConnection(); //if(process.env.NODE_ENV === 'development') // node env development
-
+establishConnection();  // node env development
 setInterval(checkActions, 10000)
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
 
 
