bb2b65b338c60926915cd188c579fc75254f9c06
trodelgal <67858041+trodelgal@users.noreply.github.com>
2020-10-09 16:42:42 +0300

    with success chalenges and submissions

--- server/api/v1/statisticsRoutes/teams.js
+++ server/api/v1/statisticsRoutes/teams.js
@@ -3,7 +3,7 @@ const sequelize = require("sequelize");
 const { Op } = require("sequelize");
 const router = Router()
 
-const { Submission, Teams, User } = require('../../../models');
+const { Submission, Teams, User, Challenge } = require('../../../models');
 
 
 router.get('/top', async (req, res) => {
@@ -36,138 +36,279 @@ router.get('/top', async (req, res) => {
   }
   })
 
-  router.get('/top-user', async (req, res) => {
-    try{
-      const thisUser= await User.findOne({
+router.get('/top-user', async (req, res) => {
+  try{
+    // let loggedUser = req.user.userId ? req.user.userId : 1
+    const userTeam= await User.findOne({
+      where:{
+        id: 1
+      },
+      include:[
+        {
+          model:Teams,
+          through:{
+            attributes:[]
+          }
+        }
+      ]
+    })
+    const teams = await Teams.findByPk(userTeam.Teams[0].id)
+    const users = await teams.getUsers({
+      raw:true,
+      group:["id"],
+      attributes:["id", "firstName", "lastName"],
+      include:[
+        {
+        model:Submission,
+        attributes:[[sequelize.fn("COUNT", sequelize.col("challenge_id")), "userSuccessSubmission"]],
         where:{
-          id:1
-        },
-        include:[
-          {
-            model:Teams,
-            through:{
-              attributes:[]
+          state:'success'
+        }
+        }
+      ]
+    })
+    users.forEach(element => {
+      delete element["UsersTeams.teamId"]
+      delete element["UsersTeams.userId"]
+      delete element["UsersTeams.createdAt"]
+      delete element["UsersTeams.updatedAt"]
+      delete element["UsersTeams.TeamId"]
+      delete element["UsersTeams.UserId"]
+    })
+    res.send(users)
+  }catch(err){
+    res.status(400).send(err)
+  }
+})
+
+router.get('/last-week-submissions', async (req, res) => {
+  try{
+    // let loggedUser = req.user.userId ? req.user.userId : 1
+    const userTeam= await User.findOne({
+      where:{
+        id: 1
+      },
+      include:[
+        {
+          model:Teams,
+          through:{
+            attributes:[]
+          }
+        }
+      ]
+    })
+    // const team = await Submission.findAll({
+    //   raw:true,
+    //   group:[sequelize.fn("DAY", sequelize.col("Submission.created_at"))],
+    //   attributes: [[sequelize.fn("COUNT", "id"), "dateSubmissions"],"createdAt"],
+    //   where:{
+    //     state:'SUCCESS',
+    //     created_at: {
+    //       [Op.gte]: new Date(Date.now() - 604800000),
+    //     }
+    //   },
+    //   include:[{
+    //     model: User,
+    //     attributes:["id"],
+    //     include:[{
+    //       model: Teams,
+    //       through:{
+    //         attributes:[]
+    //       },
+    //       attributes:["id","name"],
+    //       where:{
+    //         id:userTeam.Teams[0].id
+    //       }
+    //     }]
+    //   }],
+
+    // })
+
+    const team = await Teams.findAll({
+      // raw:true,
+      attributes: ['id','name',[sequelize.fn("COUNT", "submissions.id"), "teamSubmissions"]],
+      where:{
+        id: userTeam.Teams[0].id
+      },
+      include:[
+        {
+          model:User,
+          through:{
+            attributes:[]
+          },
+          attributes:[],
+          include:{
+            model: Submission,
+            group:[sequelize.fn("DAY", sequelize.col("Submission.created_at"))],
+            attributes:[],
+            where:{
+              state:'SUCCESS',
+              created_at: {
+                [Op.gte]: new Date(Date.now() - 604800000),
+              }
             }
           }
-        ]
-      })
-      const teams = await Teams.findByPk(thisUser.Teams[0].id)
-      const users = await teams.getUsers({
-        raw:true,
-        group:["id"],
-        attributes:["id", "firstName", "lastName"],
-        include:[
-          {
-          model:Submission,
-          attributes:[[sequelize.fn("COUNT", sequelize.col("challenge_id")), "userSuccessSubmission"]],
+        }
+      ]
+    })
+    res.send(team)
+  }catch(err){
+    res.status(400).send(err)
+  }
+})
+
+router.get("/team-submissions", async (req, res) => {
+  try{
+   // let loggedUser = req.user.userId ? req.user.userId : 1
+  const userTeam= await User.findOne({
+    where:{
+      id: 1
+    },
+    include:[
+      {
+        model:Teams,
+        through:{
+          attributes:[]
+        }
+      }
+    ]
+  })
+  const successSub = await Teams.findAll({
+    group: ["id"],
+    attributes: ['id','name'],
+    where:{
+      id:userTeam.Teams[0].id
+    },
+    include:[
+      {
+        model:User,
+        attributes:["id"],
+        through:{
+          attributes:[]
+        },
+        include:{
+          model: Submission,
+          attributes:[[sequelize.fn("COUNT", sequelize.col("challenge_id")), "teamSuccessSubmissions"]],
           where:{
             state:'success'
           }
+        }
+      }
+    ]
+  })
+  const failSub = await Teams.findAll({
+    group: ["id"],
+    attributes: ['id','name'],
+    where:{
+      id:userTeam.Teams[0].id
+    },
+    include:[
+      {
+        model:User,
+        attributes:["id"],
+        through:{
+          attributes:[]
+        },
+        include:{
+          model: Submission,
+          attributes:[[sequelize.fn("COUNT", sequelize.col("challenge_id")), "teamFailSubmissions"]],
+          where:{
+            state:'fail'
           }
-        ]
-      })
-      users.forEach(element => {
-        delete element["UsersTeams.teamId"]
-        delete element["UsersTeams.userId"]
-        delete element["UsersTeams.createdAt"]
-        delete element["UsersTeams.updatedAt"]
-        delete element["UsersTeams.TeamId"]
-        delete element["UsersTeams.UserId"]
-      })
-      res.send(users)
-    }catch(err){
-      res.status(400).send(err)
-    }
+        }
+      }
+    ]
+  })
+  const pendingSub = await Teams.findAll({
+    group: ["id"],
+    attributes: ['id','name'],
+    where:{
+      id:userTeam.Teams[0].id
+    },
+    include:[
+      {
+        model:User,
+        attributes:["id"],
+        through:{
+          attributes:[]
+        },
+        include:{
+          model: Submission,
+          attributes:[[sequelize.fn("COUNT", sequelize.col("challenge_id")), "teamPendingSubmissions"]],
+          where:{
+            state:'pending'
+          }
+        }
+      }
+    ]
+  })
+
+  const succes= successSub[0].Users[0].Submissions[0].dataValues.teamSuccessSubmissions
+  const fail = failSub[0].Users[0].Submissions[0].dataValues.teamFailSubmissions;
+  const pending = pendingSub[0].Users[0].Submissions[0].dataValues.teamPendingSubmissions;
+  const allSub= succes+fail+pending
+
+  const teamSubmissions={
+    allTheTeamSubmissions:allSub,
+    teamSuccesSubnissions:succes,
+    teamFailSubnissions:fail,
+    teamPendingSubnissions:pending
+  }
+  res.json(teamSubmissions);
+}catch(err){
+  res.json(err)
+}
+});
+
+//team success challenges
+
+router.get('/success-challenge', async (req, res) => {
+  try{
+    const userTeam= await User.findOne({
+      where:{
+        id: 1
+      },
+      include:[
+        {
+          model:Teams,
+          through:{
+            attributes:[]
+          }
+        }
+      ]
+    })
+    const successCallenges = await Teams.findAll({
+      attributes: ['id','name'],
+      where:{
+        id:userTeam.Teams[0].id
+      },
+      include:[
+        {
+          model:User,
+          attributes:["id"],
+          through:{
+            attributes:[]
+          },
+          include:{
+            model: Submission,
+            attributes:["id"],
+            where:{
+              state:'success'
+            },
+            include:[{
+              model: Challenge,
+              attributes:["name"]
+            }]
+          }
+        }
+      ]
     })
 
-    router.get('/last-week-submissions', async (req, res) => {
-      try{
-        const thisUser= await User.findOne({
-          where:{
-            id:1
-          },
-          include:[
-            {
-              model:Teams,
-              through:{
-                attributes:[]
-              }
-            }
-          ]
-        })
-        // const team = await Submission.findAll({
-        //   raw:true,
-        //   group:[sequelize.fn("DAY", sequelize.col("Submission.created_at"))],
-        //   attributes: [[sequelize.fn("COUNT", "id"), "dateSubmissions"],"createdAt"],
-        //   where:{
-        //     state:'SUCCESS',
-        //     created_at: {
-        //       [Op.gte]: new Date(Date.now() - 604800000),
-        //     }
-        //   },
-        //   include:[{
-        //     model: User,
-        //     attributes:["id"],
-        //     include:[{
-        //       model: Teams,
-        //       through:{
-        //         attributes:[]
-        //       },
-        //       attributes:["id","name"],
-        //       where:{
-        //         id:thisUser.Teams[0].id
-        //       }
-        //     }]
-        //   }],
-
-        // })
-
-        const team = await Teams.findAll({
-          raw:true,
-          attributes: ['id','name',[sequelize.fn("COUNT", "submissions.id"), "teamSubmissions"]],
-          where:{
-            id: thisUser.Teams[0].id
-          },
-          include:[
-            {
-              model:User,
-              through:{
-                attributes:[]
-              },
-              attributes:[],
-              include:{
-                model: Submission,
-                group:[sequelize.fn("DAY", sequelize.col("Submission.created_at"))],
-                attributes:[],
-                where:{
-                  state:'SUCCESS',
-                  created_at: {
-                    [Op.gte]: new Date(Date.now() - 604800000),
-                  }
-                }
-              }
-            }
-          ]
-        })
-
-        // const taem = await Submission.findAll({
-          // group:[sequelize.fn("DAY", sequelize.col("created_at"))],
-          // attributes: ['id',[sequelize.fn("COUNT", "id"), "teamSubmissions"]],
-        // })
-        // team.forEach(element => {
-        //   delete element["Users.UsersTeams.teamId"]
-        //   delete element["Users.UsersTeams.userId"]
-        //   delete element["Users.UsersTeams.createdAt"]
-        //   delete element["Users.UsersTeams.updatedAt"]
-        //   delete element["Users.UsersTeams.TeamId"]
-        //   delete element["Users.Submissions.id"]
-        //   delete element["Users.UsersTeams.UserId"]
-        // })
-        res.send(team)
-      }catch(err){
-        res.status(400).send(err)
-      }
-      })
+    res.send(successCallenges)
+  }catch(err){
+    res.status(400).send(err)
+  }
+  })
 
 module.exports = router;
 
