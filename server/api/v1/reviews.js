08f7f3d85ff355f4b85136054b8eb5c38a29c722
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-10-10 22:45:09 +0300

    Added 2 endpoints
    Can now add reviews and get reviews by user

--- server/api/v1/reviews.js
+++ server/api/v1/reviews.js
@@ -1,42 +1,57 @@
-const reviewsRouter = require("express").Router();
-const { Challenge, Review, User } = require("../../models");
+const reviewsRouter = require('express').Router();
+const { Challenge, Review, User } = require('../../models');
 
 // review mock for easy sevelopment
 const review = {
   id: 32123,
-  title: "love that challenge!!!",
+  title: 'love that challenge!!!',
   content:
-    "i had a graceful time to review this challenge and i had a lot of feedback but eventually bla bla bla",
+    'i had a graceful time to review this challenge and i had a lot of feedback but eventually bla bla bla',
   createdAt: new Date(),
   rating: 3,
 };
 
-reviewsRouter.get("/byChallenge/:challengeId", async (req, res) => {
+reviewsRouter.get('/byChallenge/:challengeId', async (req, res) => {
   try {
     const reviews = await Review.findAll({
       where: { challengeId: req.params.challengeId },
-      include:{
-        model:User, 
-        attributes:['userName']
-      }
+      include: {
+        model: User,
+        attributes: ['userName'],
+      },
     });
     res.json(reviews);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
-reviewsRouter.get("/byUser/:challengeId", async (req, res) => {
-  // TODO: get array of reviews for this user , include challenge name and id for each review object in the response
+reviewsRouter.get('/byUser/:userId', async (req, res) => {
   try {
-    res.send("reviews!!!");
+    const reviews = await Review.findAll({
+      where: { userId: req.params.userId },
+      include: {
+        model: Challenge,
+        attributes: ['name', 'id'],
+      },
+    });
+    res.send(reviews);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
-reviewsRouter.post("/:challengeId", async (req, res) => {
+reviewsRouter.post('/:challengeId', async (req, res) => {
   // TODO: (Dror maman) create a new review
+  const { title, content, rating, userId } = req.body;
+  const query = {
+    title,
+    content,
+    rating,
+    userId,
+    challengeId: req.params.challengeId,
+  };
   try {
-    res.send("reviews!!!");
+    await Review.create(query);
+    res.status(200).send("Uploaded new review!");
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
