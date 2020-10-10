489b931effb97f359a2f8af538cbf6a299a91746
AlonBru <indyalon@gmail.com>
2020-10-11 01:20:37 +0300

    fixed some inconstincies

--- client/src/components/Choosers/ChooseLabels.js
+++ client/src/components/Choosers/ChooseLabels.js
@@ -19,6 +19,8 @@ const ChooseLabels =({ submitFilter }) => {
     ,[])
   
   const selectionChange = (a)=>{
+      // allSelected=[{title:<string>,value:<string>},{title:<string>,value:<string>}]
+    // lastAction={action:<string>, option(what you clicked): {title:<string>,value:<string>} , name(name of the Selector):<string>}
     submitFilter(a?a.map(x=>x.value):[])
   }
 
