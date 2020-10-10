1ff6ffff37b279e75f1664f42b053e33baf39662
Guy Serfaty <guy@poob.me>
2020-10-10 13:42:25 +0300

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -67,7 +67,7 @@ export default function HomePage() {
   return (
     <div style={{ padding: 30, maxWidth: 840, margin: 'auto' }}>
       <Typography variant="h5" gutterBottom>
-        Challenges
+        Challenges2
       </Typography>
       {challenges.map(challenge => (
         <ChallengeCard
