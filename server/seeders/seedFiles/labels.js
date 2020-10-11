b104d40e14b00962956bc9206114d7594f636e91
AlonBru <indyalon@gmail.com>
2020-10-11 17:34:35 +0300

    Revert "added seeders for single chal"
    This reverts commit f05bd80fe3cc60078506eb6091e018ba857093f5.

--- server/seeders/seedFiles/labels.js
+++ server/seeders/seedFiles/labels.js
@@ -1,38 +1,72 @@
 module.exports = [
-    {
-      id: 1,
-      name: "React",
-      created_at: "2020-10-01 20:00:00",
-      updated_at: "2020-10-01 20:00:00",
-    },
-    {
-      id: 2,
-      name: "JavaScript",
-      created_at: "2020-10-01 20:00:00",
-      updated_at: "2020-10-01 20:00:00",
-    },
-    {
-      id: 3,
-      name: "Cyber-Security",
-      created_at: "2020-10-01 20:00:00",
-      updated_at: "2020-10-01 20:00:00",
-    },
-    {
-      id: 4,
-      name: "Authentication",
-      created_at: "2020-10-01 20:00:00",
-      updated_at: "2020-10-01 20:00:00",
-    },
-    {
-      id: 5,
-      name: "CSS",
-      created_at: "2020-10-01 20:00:00",
-      updated_at: "2020-10-01 20:00:00",
-    },
-    {
-      id: 6,
-      name: "Sequelize",
-      created_at: "2020-10-01 20:00:00",
-      updated_at: "2020-10-01 20:00:00",
-    }
-  ]
+  {
+    name: 'React',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  },
+  {
+    name: 'Node',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  },
+  {
+    name: 'Express',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  },
+  {
+    name: 'JavaScript',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  },
+  {
+    name: 'HTML',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  },
+  {
+    name: 'CSS',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  },
+  {
+    name: 'TypeScript',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  },
+  {
+    name: 'SQL',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  },
+  {
+    name: 'ORM',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  },
+  {
+    name: 'MongoDB',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  },
+  {
+    name: 'Object-Oriented',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  },
+  {
+    name: 'Secure Coding',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  },
+  {
+    name: 'Git workflow',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  },
+  {
+    name: 'Package management',
+    created_at: "2020-10-01 20:00:00",
+    updated_at: "2020-10-01 20:00:00"
+  }
+]
