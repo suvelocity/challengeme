b7a160319c8e3bac54d8fac74c44e19a3f07ab8d
AlonBru <indyalon@gmail.com>
2020-10-11 16:04:47 +0300

    packages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,20 +3,9 @@ import network from '../services/network';
 import ChallengeCard from '../components/ChallengeCard';
 import "./Home.css"
 import ThemeApi from "../services/Theme"
-import Button from '@material-ui/core/Button';
 import Cookies from "js-cookie";
 import { useHistory } from 'react-router-dom';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import network from '../services/network';
-import { Typography } from '@material-ui/core';
 import { Logged } from '../context/LoggedInContext';
-
-
 
 export default function HomePage() {
   const [challenges, setChallenges] = useState([]);
