f769130bf67e62a6264a4c92c244be73df29b2e5
trodelgal <67858041+trodelgal@users.noreply.github.com>
2020-10-11 14:29:45 +0300

    add try and catch

--- server/api/v1/statisticsRoutes/insights.js
+++ server/api/v1/statisticsRoutes/insights.js
@@ -6,46 +6,54 @@ const { Submission, Challenge, Review } = require("../../../models");
 
 // returns the 5 challenges with most submitions
 router.get("/top-challenges", async (req, res) => {
-  const sub = await Submission.findAll({
-    attributes: {
-      include: [
-        [sequelize.fn("COUNT", sequelize.col("challenge_id")), "countSub"],
-      ],
-    },
-    include: {
-      model: Challenge,
-      attributes: ["name"]
-    },
-    group: ["challenge_id"],
-    order: [[sequelize.fn("COUNT", sequelize.col("challenge_id")), "DESC"]],
-    limit: 5,
-  });
-
-  res.json(sub);
+  try{
+    const sub = await Submission.findAll({
+      attributes: {
+        include: [
+          [sequelize.fn("COUNT", sequelize.col("challenge_id")), "countSub"],
+        ],
+      },
+      include: {
+        model: Challenge,
+        attributes: ["name"]
+      },
+      group: ["challenge_id"],
+      order: [[sequelize.fn("COUNT", sequelize.col("challenge_id")), "DESC"]],
+      limit: 5,
+    });
+  
+    res.json(sub);
+  }catch(err){
+    res.json(err)
+  }
 });
 
 // returns the 5 challenges with most successful submitions
 router.get("/top-success", async (req, res) => {
-  const sub = await Submission.findAll({
-    attributes: {
-      include: [
-        [sequelize.fn("COUNT", sequelize.col("challenge_id")), "countSub"],
-      ],
-    },
-    include: {
-      model: Challenge,
-      attributes: ["name"],
-    },
-    where: { state: "SUCCESS" },
-    group: ["challenge_id"],
-    order: [[sequelize.fn("COUNT", sequelize.col("challenge_id")), "DESC"]],
-    limit: 5,
-  });
-
-  res.json(sub);
+  try{
+    const sub = await Submission.findAll({
+      attributes: {
+        include: [
+          [sequelize.fn("COUNT", sequelize.col("challenge_id")), "countSub"],
+        ],
+      },
+      include: {
+        model: Challenge,
+        attributes: ["name"],
+      },
+      where: { state: "SUCCESS" },
+      group: ["challenge_id"],
+      order: [[sequelize.fn("COUNT", sequelize.col("challenge_id")), "DESC"]],
+      limit: 5,
+    });
+    res.json(sub);
+  }catch(err){
+    res.json(err)
+  }
 });
 
 // returns the count of challenges from same type('type name' + 'count')
+<<<<<<< Updated upstream
 router.get("/challenges-type", async (req, res) => {
   const challengeType = await Challenge.findAll({
     attributes: [
@@ -57,29 +65,50 @@ router.get("/challenges-type", async (req, res) => {
     limit: 5,
   });
   res.json(challengeType);
+=======
+router.get("/challenges-category", async (req, res) => {
+  try{
+    const challengeType = await Challenge.findAll({
+      attributes: [
+        "category",
+        [sequelize.fn("COUNT", sequelize.col("type")), "countCategory"],
+      ],
+      group: ["category"],
+      order: [[sequelize.fn("COUNT", sequelize.col("category")), "DESC"]],
+      limit: 5,
+    });
+    res.json(challengeType);
+  }catch(err){
+    res.json(err)
+  }
+>>>>>>> Stashed changes
 });
 
 // returns the count of submitions submited per day from the last 5 days 
 router.get("/sub-by-date", async (req, res) => {
-  const subByDate = await Submission.findAll({
-    group: [sequelize.fn("DAY", sequelize.col("createdAt"))],
-    attributes: [
-      [sequelize.fn("COUNT", sequelize.col("id")), "countByDay"],
-      "createdAt",
-    ],
-    where: {
-      created_at: {
-        //432000000
-        [Op.gte]: new Date(Date.now() - 2.628e+9),
+  try{
+    const subByDate = await Submission.findAll({
+      group: [sequelize.fn("DAY", sequelize.col("createdAt"))],
+      attributes: [
+        [sequelize.fn("COUNT", sequelize.col("id")), "countByDay"],
+        "createdAt",
+      ],
+      where: {
+        created_at: {
+          //432000000
+          [Op.gte]: new Date(Date.now() - 2.628e+9),
+        },
       },
-    },
-  });
-  res.json(subByDate);
+    });
+    res.json(subByDate);
+  }catch(err){
+    res.json(err)
+  }
 });
 
 // returns top 5 challenges ordered by rating averge (from reviews) 
 router.get("/challenges-by-reviews", async (req, res) => {
-
+try{
   let allChallenges = await Review.findAll({
     attributes: [[sequelize.fn('AVG', sequelize.col('rating')), 'ratingAVG']],
     include: {
@@ -98,6 +127,9 @@ router.get("/challenges-by-reviews", async (req, res) => {
   });
 
   res.json(allChallenges)
+}catch(err){
+  res.json(err)
+}
 });
 
 module.exports = router;
