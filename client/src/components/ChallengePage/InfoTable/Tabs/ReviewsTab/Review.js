3904facf3e2cf67866a5870e9ecc47128dc59e2e
OriSass <67857592+OriSass@users.noreply.github.com>
2020-10-11 12:10:54 +0300

    prep for solution repo by user

--- client/src/components/ChallengePage/InfoTable/Tabs/ReviewsTab/Review.js
+++ client/src/components/ChallengePage/InfoTable/Tabs/ReviewsTab/Review.js
@@ -3,19 +3,22 @@ import Rating from "@material-ui/lab/Rating";
 import './Review.css';
 
 
-function Review({ author, createdAt, title, content, rating }) {
+function Review({ author, createdAt, title, content, rating, 
+	// solutionRepo
+ }) {
 	return (
 		<div class="review">
 			<div id = "title">{title}</div>
 			<div id = "createdAt">{createdAt.split("T")[0]}</div>
 			<div id = "content">{content}</div>
+			{/* {solutionRepo ? <div id = "solution-repo">{solutionRepo}</div> : <></>} */}
 			<div id="rating">
           		<Rating
             		name="half-rating-read"
             		defaultValue={rating}
             		precision={0.5}
             		readOnly/>
-        </div>
+        	</div>
 			<div id = "author">By: {author}</div>
 		</div>
 	);
--- client/src/components/ChallengePage/InfoTable/Tabs/ReviewsTab/ReviewsTab.js
+++ client/src/components/ChallengePage/InfoTable/Tabs/ReviewsTab/ReviewsTab.js
@@ -1,46 +1,41 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import Review from "./Review";
-import network from "../../../../../services/network";
-const challengeId = 2;
-function ReviewsTab({ challengeId }) {
-  const [reviews, setReviews] = useState(null);
-  useEffect(() => {
-    const fetchReviews = async () => {
-      const { data: reviewsArrayFromServer } = await network.get(
-        `/api/v1/reviews/byChallenge/${challengeId}`
-      );
-      const reviewsWithContent = reviewsArrayFromServer.filter(
-        (review) => review.title && review.content
-      );
-      setReviews(reviewsWithContent);
-    };
-    fetchReviews();
-    const liveReviews = setInterval(fetchReviews, 5000);
-    return () => clearInterval(liveReviews);
-  }, [challengeId]);
-  return reviews ? (
-    reviews.map((review, index) => {
-      const {
-        createdAt,
-        title,
-        content,
-        rating,
-        User: { userName },
-      } = review;
-      return (
-        <Review
-          author={userName}
-          createdAt={createdAt}
-          title={title}
-          content={content}
-          rating={rating}
-        />
-      );
-    })
-  ) : (
-    <h1>Loading....</h1>
-  );
+import React from 'react'
+import { useEffect, useState } from 'react';
+import Review from './Review';
+import network from '../../../../../services/network';
+const userId = 1;
+const challengeId = 2 ;
+function ReviewsTab({challengeId}) {
+    // TODO: fetch interval all reviews
+    // TODO: change the style with amit
+    // TODO:insert a review with dror and expect to be here
+    // TODO: user can delete his review, bonus , can edit it
+    const [reviews,setReviews] = useState(null)
+    useEffect(() => {
+        const fetchReviews = async () => {
+            const {data: reviewsArray } = await network.get(`/api/v1/reviews/byChallenge/${challengeId}`)
+        const filteredReviews = reviewsArray.filter(review => review.content && review.title)
+        setReviews(filteredReviews);
+        }
+        fetchReviews()
+        const liveReviews = setInterval(fetchReviews,5000)
+        return () => clearInterval(liveReviews)
+    },[challengeId])
+    return reviews ?  (
+        reviews.map((review,index)=>{
+                const { createdAt, title, content, rating,User:{userName} } = review;
+                return  <Review
+                author={userName}
+                createdAt={createdAt} 
+                title={title}
+                content={content}
+                rating={rating}
+                solutionRepo={solutionRepo}
+                />
+            }))
+            :(
+            <h1>Loading....</h1>
+            )
+    
 }
 
 export default ReviewsTab;
