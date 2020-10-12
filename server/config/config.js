d93cb588a91b4e9b8d6c698d675a06663ed02a58
Guy Serfaty <guy@poob.me>
2020-10-13 01:15:39 +0300

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ module.exports = {
   production: {
     username: process.env.DB_USERNAME || "root",
     password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME || "challenge_production",
+    database: process.env.DB_NAME || "challenge",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql",
     define: { underscored: true },
