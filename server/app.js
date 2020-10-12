f93e70fbf26232939e667b95e1bfba95c6fb2258
Guy Serfaty <guy@poob.me>
2020-10-13 00:38:30 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,13 +8,14 @@ app.use(express.json( {limit: '50mb'}))
 
 app.use('/api', require('./api')) // if auth works this can probably go
 
-app.use('*', function(req,res){
-    res.sendStatus(404)
-})
-
 app.use(express.static(path.join(__dirname, "..", "client", 'build')))
 app.use('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
+
+app.use('*', function(req,res){
+    res.sendStatus(404)
+})
+
 
 module.exports = app;
