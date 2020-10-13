a1b1718316493f096ae7b652803126e9a01f9531
david35008 <67857450+david35008@users.noreply.github.com>
2020-10-13 20:06:39 +0300

    add logo

--- server/index.js
+++ server/index.js
@@ -12,8 +12,6 @@ async function establishConnection(){
 }
 establishConnection(); 
 
-// setInterval(checkActions, 10000)
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
