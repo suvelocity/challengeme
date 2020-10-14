af1f9642ab1dc7e2e3f40b8d4bc84af78a510350
AlonBru <indyalon@gmail.com>
2020-10-14 23:57:58 +0300

    Update labelsToChallenges.js
    added ids

--- server/seeders/seedData/labelsToChallenges.js
+++ server/seeders/seedData/labelsToChallenges.js
@@ -1,60 +1,70 @@
 module.exports = [
     {
-        challenge_id: 1,
+      id:1,  
+      challenge_id: 1,
         label_id: 1,
         created_at: "2020-10-01 20:00:00",
         updated_at: "2020-10-01 20:00:00"
     },
     {
-        challenge_id: 1,
+      id:2,  
+      challenge_id: 1,
         label_id: 2,
         created_at: "2020-10-01 20:00:00",
         updated_at: "2020-10-01 20:00:00"
     },
     {
-        challenge_id: 1,
+      id:3,  
+      challenge_id: 1,
         label_id: 3,
         created_at: "2020-10-01 20:00:00",
         updated_at: "2020-10-01 20:00:00"
     },
     {
-        challenge_id: 2,
+      id:4,  
+      challenge_id: 2,
         label_id: 1,
         created_at: "2020-10-01 20:00:00",
         updated_at: "2020-10-01 20:00:00"
     },
     {
-        challenge_id: 2,
+      id:5,  
+      challenge_id: 2,
         label_id: 4,
         created_at: "2020-10-01 20:00:00",
         updated_at: "2020-10-01 20:00:00"
     },
     {
-        challenge_id: 2,
+      id:6,  
+      challenge_id: 2,
         label_id: 5,
         created_at: "2020-10-01 20:00:00",
         updated_at: "2020-10-01 20:00:00"
     },
     {
-        challenge_id: 3,
+      id:7,  
+      challenge_id: 3,
         label_id: 5,
         created_at: "2020-10-01 20:00:00",
         updated_at: "2020-10-01 20:00:00"
     },
     {
-        challenge_id: 3,
+      id:8,  
+      challenge_id: 3,
         label_id: 6,
         created_at: "2020-10-01 20:00:00",
         updated_at: "2020-10-01 20:00:00"
     },
     {
-        challenge_id: 3,
+      id:9,  
+      challenge_id: 3,
         label_id: 7,
         created_at: "2020-10-01 20:00:00",
         updated_at: "2020-10-01 20:00:00"
     },
     {
-        challenge_id: 3,
+      id:10,  
+      challenge_id: 3,
         label_id: 8,
         created_at: "2020-10-01 20:00:00",
         updated_at: "2020-10-01 20:00:00"
