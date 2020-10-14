00aa2216b428d1468335df7a934a83b35c0b5014
david35008 <67857450+david35008@users.noreply.github.com>
2020-10-14 20:08:10 +0300

    refactor lablels

--- server/api/v1/labels.js
+++ server/api/v1/labels.js
@@ -3,7 +3,6 @@ const { LabelChallenge, Label } = require("../../models");
 const router = Router();
 
 router.post("/", async (req, res) => {
-  // /api/v1/labels
   let { challengeId } = req.body;
   let { labels: labelsArray } = req.body;
   if (labels.length > 0) {
@@ -16,23 +15,26 @@ router.post("/", async (req, res) => {
       );
       res.json({ message: "lables created successfully" });
     } catch (error) {
-      res.status(400).send("Bad request");
+      res.status(400).json({ message: "Cannot process request" })
     }
   } else {
-    res.status(400).send("No labels chosen");
+    res.status(400).json({ message: "No labels chosen" });
   }
 });
 
 
 // get all label
 router.get("/", async (req, res) => {
-  // TODO:  reloacte to label route
-  const allLabels = await Label.findAll();
-  res.json(
-    allLabels.map(({ id, name }) => {
-      return { label: name, value: id };
-    })
-  );
+  try {
+    const allLabels = await Label.findAll();
+    res.json(
+      allLabels.map(({ id, name }) => {
+        return { label: name, value: id };
+      })
+    );
+  } catch (error) {
+    res.status(400).json({ message: "Cannot process request" })
+  }
 });
 
 module.exports = router;
