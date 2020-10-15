3f9920f5f0f8cd443e053e5d036c400f0e41dc87
tsach ovadia <67857438+ItsDrTsach@users.noreply.github.com>
2020-10-16 02:07:53 +0300

    added TODO's

--- client/src/components/ChallengePage/mainPage/ChallengePage.js
+++ client/src/components/ChallengePage/mainPage/ChallengePage.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import network from '../../../services/network';
-import { Button } from '@material-ui/core';
-import Rating from '@material-ui/lab/Rating';
-import Chip from '@material-ui/core/Chip';
-import './ChallengePage.css';
-import SubmitModal from '../SubmitModal';
-import InfoTable from '../InfoTable/InfoTable';
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import network from "../../../services/network";
+import { Button } from "@material-ui/core";
+import Rating from "@material-ui/lab/Rating";
+import Chip from "@material-ui/core/Chip";
+import "./ChallengePage.css";
+import SubmitModal from "../SubmitModal";
+import InfoTable from "../InfoTable/InfoTable";
 
 // TODO: clean all console alert and logs before merge
 
@@ -16,16 +16,16 @@ import InfoTable from '../InfoTable/InfoTable';
 
 function normalizeDate(dateTime) {
   //"2020-10-04T12:00:00.000Z";
-  const date = dateTime.split('T')[0];
+  const date = dateTime.split("T")[0];
   return date;
 }
 
 export default function ChallengePage() {
   const [challenge, setChallenge] = useState(null);
   const { challengeParamId } = useParams();
-  const [blobedImg, setBlobedImg] = useState('');
+  const [image, setImage] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [darkModeIsOn] = useState(false);
+  const [darkModeIsOn] = useState(false); // TODO: subscribe to global state , create recoil Atom to the state
 
   useEffect(() => {
     const setImg = async () => {
@@ -33,7 +33,7 @@ export default function ChallengePage() {
         const { data } = await network.get(
           `/api/v1/image?id=${challengeParamId}`
         );
-        setBlobedImg(data.img);
+        setImage(data.img);
       } catch (error) {
         console.error(error);
       }
@@ -43,7 +43,7 @@ export default function ChallengePage() {
         let {
           data: { challenge: challengeFromServer, author },
         } = await network.get(`/api/v1/challenges/${challengeParamId}`);
-        challengeFromServer.author = author;
+        challengeFromServer.author = author; // TODO: format request to the server api...
         setChallenge(challengeFromServer);
       } catch (error) {
         console.error(error);
@@ -58,91 +58,90 @@ export default function ChallengePage() {
   }
 
   return challenge ? (
-    <div className='fullpage-wrapper'>
+    <div className="fullpage-wrapper">
       <div
         className={
-          darkModeIsOn ? 'dark-challenge-wrapper' : 'light-challenge-wrapper'
+          darkModeIsOn ? "dark-challenge-wrapper" : "light-challenge-wrapper"
         }
       >
-        <div className='challenge-left-wrapper'>
-          <div className='challenge-img-div'>
-            <img
-              className='challenge-img'
-              src={blobedImg}
-              alt={challenge.name}
-            />
+        <div className="challenge-left-wrapper">
+          <div className="challenge-img-div">
+            <img className="challenge-img" src={image} alt={challenge.name} />
           </div>
           <div
             className={
-              darkModeIsOn ? 'challenge-rawdata-dm' : 'challenge-rawdata'
+              darkModeIsOn ? "challenge-rawdata-dm" : "challenge-rawdata"
             }
           >
-            <span className='challenge-created-by'>
-              {/* <p>Created By: </p> <p>{challenge.author.userName}</p> */}
+            <span className="challenge-created-by">
+              {/* TODO: fix that <p>Created By: </p> <p>{challenge.author.userName}</p>  */}
             </span>
-            <span className='challenge-created-at'>
-              <p>Created At: </p>{' '}
-              <p>{normalizeDate(challenge.createdAt) + ' '} </p>
+            <span className="challenge-created-at">
+              <p>Created At: </p>{" "}
+              <p>{normalizeDate(challenge.createdAt) + " "} </p>
             </span>
-            <span className='challenge-updated-at'>
+            <span className="challenge-updated-at">
               <p>Updated At: </p> <p>{normalizeDate(challenge.updatedAt)}</p>
             </span>
           </div>
           <div
             className={
-              darkModeIsOn ? 'challenge-labels-dm' : 'challenge-labels'
+              darkModeIsOn ? "challenge-labels-dm" : "challenge-labels"
             }
           >
-            <h2 className={darkModeIsOn ? 'dark-h2' : 'light-h2'}>Labels:</h2>
-            <span className='challenge-label'>
-              {challenge['Lbaels'] && challenge['Labels'].map((label) => (
-                <Link to={`/?labelId=${label['labels_to_challenges']['label_id']}`}>
-                  <Chip color='primary' label={label.name} component='a' />
-                </Link>
-              ))}
+            <h2 className={darkModeIsOn ? "dark-h2" : "light-h2"}>Labels:</h2>
+            <span className="challenge-label">
+              {challenge["Labels"] &&
+                challenge["Labels"].map((label) => (
+                  <Link
+                    to={`/?labelId=${label["labels_to_challenges"]["label_id"]}`}
+                  >
+                    <Chip color="primary" label={label.name} component="a" />
+                  </Link>
+                ))}
             </span>
           </div>
-          <div className='challenge-rating'>
-            <h2 className={darkModeIsOn ? 'dark-h2' : 'light-h2'}>Rating:</h2>
+          <div className="challenge-rating">
+            <h2 className={darkModeIsOn ? "dark-h2" : "light-h2"}>Rating:</h2>
             <Rating
-              name='half-rating-read'
+              name="half-rating-read"
               defaultValue={5}
               precision={0.5}
               readOnly
-              size='large'
+              size="large"
             />
           </div>
           <div
             className={
-              darkModeIsOn ? 'challenge-github-btn-dm' : 'challenge-github-btn'
+              darkModeIsOn ? "challenge-github-btn-dm" : "challenge-github-btn"
             }
           >
             <Button
-              color='primary'
+              color="primary"
               href={`https://github.com/${challenge.boilerPlate}`}
             >
               Fork boiler plate
             </Button>
           </div>
         </div>
-        <div className='challenge-right-wrapper'>
-          <div className='challenge-title-description'>
-            <div className='challenge-name'>
-              <h1 className={darkModeIsOn ? 'dark-h1' : 'light-h1'}>
+        <div className="challenge-right-wrapper">
+          <div className="challenge-title-description">
+            <div className="challenge-name">
+              <h1 className={darkModeIsOn ? "dark-h1" : "light-h1"}>
                 {challenge.name}
               </h1>
             </div>
             <div
               className={
                 darkModeIsOn
-                  ? 'challenge-description-dm'
-                  : 'challenge-description'
+                  ? "challenge-description-dm"
+                  : "challenge-description"
               }
             >
               <p>{challenge.description}</p>
             </div>
           </div>
-          <div className='challenge-solution-table'>
+          <div className="challenge-solution-table">
             <InfoTable
               challengeId={challengeParamId}
               darkModeIsOn={darkModeIsOn}
@@ -150,10 +149,10 @@ export default function ChallengePage() {
           </div>
           <div
             className={
-              darkModeIsOn ? 'challenge-submit-btn-dm' : 'challenge-submit-btn'
+              darkModeIsOn ? "challenge-submit-btn-dm" : "challenge-submit-btn"
             }
           >
-            <Button color='primary' onClick={() => setIsModalOpen(true)}>
+            <Button color="primary" onClick={() => setIsModalOpen(true)}>
               Submit
             </Button>
           </div>
@@ -167,6 +166,6 @@ export default function ChallengePage() {
       />
     </div>
   ) : (
-      <div>Loading</div>
-    );
+    <div>Loading</div>
+  );
 }
