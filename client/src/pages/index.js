e335829bcacc6ca64efa4d2b5d43b89994eb17f9
ShaharEli <shahar.e3@gmail.com>
2020-10-16 01:06:56 +0300

    error boundris added

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, lazy, Suspense } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import ThemeApi from '../services/Theme'
-import Statistics from './Statistics';
-import { Logged } from '../context/LoggedInContext';
-import Register from './Register/Register';
-import Login from './Login';
-import Cookies from 'js-cookie';
-import Forgot from './Forgot/Forgot';
-import VlidatingMail from './Register/VlidatingMail';
-import network from '../services/network';
-import Landing from './Landing';
-import { AnimatePresence } from 'framer-motion';
+import ThemeApi from "../services/Theme";
+import Statistics from "./Statistics";
+import { Logged } from "../context/LoggedInContext";
+import Register from "./Register/Register";
+import Login from "./Login";
+import Cookies from "js-cookie";
+import Forgot from "./Forgot/Forgot";
+import VlidatingMail from "./Register/VlidatingMail";
+import network from "../services/network";
+import Landing from "./Landing";
+import { AnimatePresence } from "framer-motion";
 import ChallengePage from "../components/ChallengePage/mainPage/ChallengePage";
-import Home from './Home/Home'; // post- Shahar folder revolution
-import NewChallengeForm from '../components/NewChallenge/NewChallengeForm';
-import Header from '../components/Header/Header'
+import Home from "./Home/Home"; // post- Shahar folder revolution
+import NewChallengeForm from "../components/NewChallenge/NewChallengeForm";
+import Header from "../components/Header/Header";
+import ChallengeErrorBoundry from "../services/ChallengeErrorBoundry";
+import HomeErrorBoundry from "../services/HomeErrorBoundry";
+import AuthErrorBoundry from "../services/AuthErrorBoundry";
 
 export default function Router() {
   const [darkTheme, setDarkTheme] = useState(false);
@@ -22,32 +25,31 @@ export default function Router() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const previousTheme = localStorage.getItem('darkMode'); //get previous selected theme
-    if (previousTheme === 'false') {
+    const previousTheme = localStorage.getItem("darkMode"); //get previous selected theme
+    if (previousTheme === "false") {
       setDarkTheme(false);
-    } else if (previousTheme === 'true') {
+    } else if (previousTheme === "true") {
       setDarkTheme(true);
     } else {
-      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
         //check default theme of the user
         setDarkTheme(true);
       }
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
-    
-    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-      setDarkTheme(true)
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setDarkTheme(true);
     }
-  },[])
+  }, []);
 
   useEffect(() => {
     // auth
     (async () => {
-      if (Cookies.get('accessToken')) {
+      if (Cookies.get("accessToken")) {
         try {
-          const { data } = await network.get('/api/v1/auth/validateToken');
+          const { data } = await network.get("/api/v1/auth/validateToken");
           setLogged(data);
           setLoading(false);
         } catch (e) {
@@ -65,30 +67,32 @@ export default function Router() {
         !logged ? (
           <Logged.Provider value={{ logged, setLogged }}>
             <AnimatePresence>
-              <Route
-                render={({ location }) => (
-                  <Switch location={location} key={location.pathname}>
-                    <Route exact path='/register'>
-                      <Register />
-                    </Route>
-                    <Route exact path='/login'>
-                      <Login />
-                    </Route>
-                    <Route exact path='/forgot'>
-                      <Forgot />
-                    </Route>
-                    <Route exact path='/auth'>
-                      <VlidatingMail />
-                    </Route>
-                    <Route exact path='/'>
-                      <Landing />
-                    </Route>
-                    <Route path='*'>
-                      <Redirect to='/' />
-                    </Route>
-                  </Switch>
-                )}
-              />
+              <AuthErrorBoundry>
+                <Route
+                  render={({ location }) => (
+                    <Switch location={location} key={location.pathname}>
+                      <Route exact path='/register'>
+                        <Register />
+                      </Route>
+                      <Route exact path='/login'>
+                        <Login />
+                      </Route>
+                      <Route exact path='/forgot'>
+                        <Forgot />
+                      </Route>
+                      <Route exact path='/auth'>
+                        <VlidatingMail />
+                      </Route>
+                      <Route exact path='/'>
+                        <Landing />
+                      </Route>
+                      <Route path='*'>
+                        <Redirect to='/' />
+                      </Route>
+                    </Switch>
+                  )}
+                />
+              </AuthErrorBoundry>
             </AnimatePresence>
           </Logged.Provider>
         ) : (
@@ -96,21 +100,29 @@ export default function Router() {
             <ThemeApi.Provider value={{ darkTheme, setDarkTheme }}>
               <Header />
               <Switch>
-                <Route exact path='/'>
-                  <Home />
-                </Route>
-                <Route path='/add_challenge'>
-                  <NewChallengeForm />
-                </Route>
+                <HomeErrorBoundry>
+                  <Route exact path='/'>
+                    <Home />
+                  </Route>
+                </HomeErrorBoundry>
+                <AddChallengeErrorBoundry>
+                  <Route path='/add_challenge'>
+                    <NewChallengeForm />
+                  </Route>
+                </AddChallengeErrorBoundry>
                 <Route path='/statistics'>
                   <Statistics />
                 </Route>
-                <Route exact path='/challenges/:challengeParamId'>
-                      <ChallengePage />
-                    </Route>
-                <Route path='*'>
-                  <Redirect to='/' />
-                </Route>
+                <ChallengeErrorBoundry>
+                  <Route exact path='/challenges/:challengeParamId'>
+                    <ChallengePage />
+                  </Route>
+                </ChallengeErrorBoundry>
+                <HomeErrorBoundry>
+                  <Route path='*'>
+                    <Redirect to='/' />
+                  </Route>
+                </HomeErrorBoundry>
               </Switch>
             </ThemeApi.Provider>
           </Logged.Provider>
