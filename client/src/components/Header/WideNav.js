be129cf294676f0f5bce11a29bb9107c47027829
RoyShnitzel <67857836+RoyShnitzel@users.noreply.github.com>
2020-10-20 21:49:09 +0300

    fixed label filter visbilty when not in homepage

--- client/src/components/Header/WideNav.js
+++ client/src/components/Header/WideNav.js
@@ -15,242 +15,253 @@ import "./Header.css";
 import DarkModeToggle from "react-dark-mode-toggle";
 import Search from "./Search/Search";
 import { Logged } from "../../context/LoggedInContext";
-import FilteredLabels from '../../context/FilteredLabelsContext';
-import { useHistory } from "react-router-dom";
+import FilteredLabels from "../../context/FilteredLabelsContext";
+import { useHistory, useLocation } from "react-router-dom";
 import network from "../../services/network";
 import ChooseLabels from "../Choosers/ChooseLabels";
 
 const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
-    appBarRegolar: {
-        transition: theme.transitions.create(["margin", "width"], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(["margin", "width"], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    darkModeToggle: {
-        marginRight: "10px;",
-    },
-    infoButton: {
-        margin: "10px",
-    },
-    logOutButton: {
-        margin: "10px",
-    },
-    root: {
-        display: "flex",
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    hide: {
-        display: "none",
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    generalDrawerHeader: {
-        display: "flex",
-    },
-    drawerHeader: {
-        marginLeft: "auto",
-        display: "flex",
-        alignItems: "center",
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-        justifyContent: "flex-end",
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-        transition: theme.transitions.create("margin", {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginLeft: -drawerWidth,
-    },
-    contentShift: {
-        transition: theme.transitions.create("margin", {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
-    },
-    list: {
-        padding: 0,
-    },
-    avatarUserInfo: {
-        margin: "20px",
-    },
-    filterButton: {
-        backgroundColor: "rgb(219,219,219)",
-        marginLeft: "10px",
-    },
-    filterButtonDark: {
-        backgroundColor: "rgb(81,81,81)",
-        color: "white",
-        marginLeft: "10px",
-    },
+  appBarRegolar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  darkModeToggle: {
+    marginRight: "10px;",
+  },
+  infoButton: {
+    margin: "10px",
+  },
+  logOutButton: {
+    margin: "10px",
+  },
+  root: {
+    display: "flex",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  generalDrawerHeader: {
+    display: "flex",
+  },
+  drawerHeader: {
+    marginLeft: "auto",
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  list: {
+    padding: 0,
+  },
+  avatarUserInfo: {
+    margin: "20px",
+  },
+  filterButton: {
+    backgroundColor: "rgb(219,219,219)",
+    marginLeft: "10px",
+  },
+  filterButtonDark: {
+    backgroundColor: "rgb(81,81,81)",
+    color: "white",
+    marginLeft: "10px",
+  },
 }));
 
 export default function WideNav({ darkMode, setDarkMode }) {
-    const filteredLabels = useContext(FilteredLabels);
-    const classes = useStyles();
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const location = useHistory();
-    const value = useContext(Logged);
-    const [labels, setLabels] = useState([]);
-    const [chooseLabels, setChooseLabels] = useState([]);
+  const filteredLabels = useContext(FilteredLabels);
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const location = useHistory();
+  const value = useContext(Logged);
+  const [labels, setLabels] = useState([]);
+  const [chooseLabels, setChooseLabels] = useState([]);
+  const currentLocation = useLocation();
 
+  console.log(currentLocation);
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    const handleMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const logOut = async () => {
+    try {
+      await network.post("/api/v1/auth/logout", {
+        token: Cookies.get("refreshToken"),
+      });
+      Cookies.remove("refreshToken");
+      Cookies.remove("accessToken");
+      Cookies.remove("name");
+      Cookies.remove("userId");
+      value.setLogged(false);
+      location.push("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const logOut = async () => {
-        try {
-            await network.post("/api/v1/auth/logout", {
-                token: Cookies.get("refreshToken"),
-            });
-            Cookies.remove("refreshToken");
-            Cookies.remove("accessToken");
-            Cookies.remove("name");
-            Cookies.remove("userId");
-            value.setLogged(false);
-            location.push("/login");
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    const headerStyle = {
-        backgroundColor: darkMode ? "rgb(51,51,51)" : "white",
-    };
-    const letterColor = {
-        color: darkMode ? "white" : "black",
-    };
-    return (
-        <AppBar
-            position="fixed"
-            className={clsx(classes.appBarRegolar)}
-            style={headerStyle}
+  const headerStyle = {
+    backgroundColor: darkMode ? "rgb(51,51,51)" : "white",
+  };
+  const letterColor = {
+    color: darkMode ? "white" : "black",
+  };
+  return (
+    <AppBar
+      position="fixed"
+      className={clsx(classes.appBarRegolar)}
+      style={headerStyle}
+    >
+      <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+          <NavLink to="/" exact className="link-rout">
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginRight: "10px",
+              }}
+            >
+              <HomeIcon style={letterColor} />
+              &nbsp;
+              <span style={letterColor} className="header-link-title">
+                ChallengeMe
+              </span>
+            </div>
+          </NavLink>
+        </Typography>
+        <Search darkMode={darkMode} setDarkMode={setDarkMode} />
+        <div style={{ minWidth: "150px", width: "fit-content" }}>
+          {currentLocation.pathname === "/" ? (
+            <ChooseLabels
+              labels={labels}
+              chooseLabels={chooseLabels}
+              setChooseLabels={setChooseLabels}
+              darkMode={darkMode}
+              submitFilter={setLabels}
+            />
+          ) : null}
+        </div>
+        {currentLocation.pathname === "/" ? (
+          <Button
+            onClick={() => {
+              filteredLabels.setFilteredLabels(
+                labels ? labels.map((label) => label.value) : []
+              );
+              setLabels([]);
+            }}
+            variant="contained"
+            className={
+              darkMode ? classes.filterButtonDark : classes.filterButton
+            }
+          >
+            filter
+          </Button>
+        ) : null}
+        <div style={{ flex: 1 }}></div>
+        {/* Make space between the search input and the rest of the header. */}
+        <DarkModeToggle
+          className={classes.darkModeToggle}
+          checked={darkMode}
+          onChange={() => {
+            localStorage.setItem("darkMode", !darkMode);
+            setDarkMode((prev) => !prev);
+          }}
+          size={45}
+        />
+        <Tooltip title={Cookies.get("name")}>
+          <Avatar
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+            style={{
+              cursor: "pointer",
+              backgroundColor: darkMode ? "rgb(140,110,99)" : "#7BACB4",
+            }}
+          >
+            {Cookies.get("name").slice(0, 2)}
+          </Avatar>
+        </Tooltip>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          className={classes.menu}
         >
-            <Toolbar>
-                <Typography variant="h6" className={classes.title}>
-                    <NavLink
-                        to="/"
-                        exact
-                        className="link-rout"
-                    >
-                        <div
-                            style={{
-                                display: "flex",
-                                alignItems: "center",
-                                marginRight: "10px",
-                            }}
-                        >
-                            <HomeIcon style={letterColor} />
-                            &nbsp;
-                            <span style={letterColor} className="header-link-title">
-                                ChallengeMe
-                            </span>
-                        </div>
-                    </NavLink>
-                </Typography>
-                <Search darkMode={darkMode} setDarkMode={setDarkMode} />
-                <div style={{ minWidth: "150px", width: "fit-content" }}>
-                    <ChooseLabels labels={labels} chooseLabels={chooseLabels} setChooseLabels={setChooseLabels} darkMode={darkMode} submitFilter={setLabels} />
-                </div>
-                <Button
-                    onClick={() => {
-                        filteredLabels.setFilteredLabels(labels ? labels.map(label => label.value) : [])
-                        setLabels([])
-                    }}
-                    variant="contained"
-                    className={darkMode ? classes.filterButtonDark : classes.filterButton}
-                >
-                    filter
-                    </Button>
-                <div style={{ flex: 1 }}></div>
-                {/* Make space between the search input and the rest of the header. */}
-                <DarkModeToggle
-                    className={classes.darkModeToggle}
-                    checked={darkMode}
-                    onChange={() => {
-                        localStorage.setItem("darkMode", !darkMode);
-                        setDarkMode((prev) => !prev);
-                    }}
-                    size={45}
-                />
-                <Tooltip title={Cookies.get("name")}>
-                    <Avatar
-                        aria-label="account of current user"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        onClick={handleMenu}
-                        color="inherit"
-                        style={{
-                            cursor: "pointer",
-                            backgroundColor: darkMode ? "rgb(140,110,99)" : "#7BACB4",
-                        }}
-                    >
-                        {Cookies.get("name").slice(0, 2)}
-                    </Avatar>
-                </Tooltip>
-                <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                        vertical: "bottom",
-                        horizontal: "center",
-                    }}
-                    transformOrigin={{
-                        vertical: "top",
-                        horizontal: "right",
-                    }}
-                    keepMounted
-                    open={open}
-                    onClose={handleClose}
-                    className={classes.menu}
-                >
-                    <Link to="/user_info" className="link-rout">
-                        <Button
-                            className={classes.infoButton}
-                            style={{ minWidth: 150 }}
-                            variant="contained"
-                            color="default"
-                        >
-                            info
-                        </Button>
-                    </Link>
-                    <Button
-                        className={classes.logOutButton}
-                        onClick={logOut}
-                        style={{ minWidth: 150 }}
-                        variant="contained"
-                        color="secondary"
-                    >
-                        Log Out
-                    </Button>
-                </Menu>
-            </Toolbar>
-        </AppBar>
-    );
+          <Link to="/user_info" className="link-rout">
+            <Button
+              className={classes.infoButton}
+              style={{ minWidth: 150 }}
+              variant="contained"
+              color="default"
+            >
+              info
+            </Button>
+          </Link>
+          <Button
+            className={classes.logOutButton}
+            onClick={logOut}
+            style={{ minWidth: 150 }}
+            variant="contained"
+            color="secondary"
+          >
+            Log Out
+          </Button>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
 }
