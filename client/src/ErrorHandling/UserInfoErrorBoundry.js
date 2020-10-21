c7d1f1726eeadf1d27391be5b7da7299cd675ebf
Cyber4sPopo <69596366+Cyber4sPopo@users.noreply.github.com>
2020-10-21 20:03:43 +0300

    Update UserInfoErrorBoundry.js

--- client/src/ErrorHandling/UserInfoErrorBoundry.js
+++ client/src/ErrorHandling/UserInfoErrorBoundry.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+// TODO: POPO: Create one generic component instead of have ~5 component (ErrorHandling) that does the same, this is not jquery.
 export default class UserInfoErrorBoundry extends Component {
   constructor(props) {
     super(props);
