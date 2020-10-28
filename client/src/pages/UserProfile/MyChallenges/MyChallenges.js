db4b1ca5d802686308368536df600b26c0a208e8
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-10-28 20:52:24 +0200

    design newChallenge and user Profile

--- client/src/pages/UserProfile/MyChallenges/MyChallenges.js
+++ client/src/pages/UserProfile/MyChallenges/MyChallenges.js
@@ -1,48 +1,69 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import network from "../../../services/network";
+import "./MyChallenges.css";
+import Button from "@material-ui/core/Button";
+
 
 const UserProfileLanding = () => {
-
-    const [allMyChallenges, setAllMyChallenges] = useState([])
+    const [allMyChallenges, setAllMyChallenges] = useState([]);
 
     const getMyChallenges = async () => {
-        const { data: allMyChallengesFromServer } = await network.get('/api/v1/challenges/userChallenges')
-        setAllMyChallenges(allMyChallengesFromServer)
-    }
+        const { data: allMyChallengesFromServer } = await network.get(
+            "/api/v1/challenges/userChallenges"
+        );
+        setAllMyChallenges(allMyChallengesFromServer);
+    };
     useEffect(() => {
-        getMyChallenges()
-    }, [])
+        getMyChallenges();
+    }, []);
 
     return (
-        <div style={{ paddingTop: '200px' }} >
-            <h1>This is challenges page</h1>
-            <Link to='/profile' >
-                myprofile
-            </Link>
-            {allMyChallenges && allMyChallenges.map((challenge) => {
-                return (
-                    <div key={challenge.name + challenge.id} >
-                        <h1>Name: {challenge.name}</h1>
-                        <div>Id :{challenge.id}</div>
-                        <div>Author :{challenge.Author.userName}</div>
-                        <div>Author Id: {challenge.authorId}</div>
-                        <div>Labels: {challenge.Labels ? challenge.Labels.map((label) => {
-                            return <li key={label.name} >{label.name}</li>
-                        }) : <h2>Labels Not Found</h2>}</div>
-                        <div>Boiler Plate: {challenge.boilerPlate}</div>
-                        <div>Repository Name: {challenge.repositoryName}</div>
-                        <div>Description: {challenge.description}</div>
-                        <div>State: {challenge.state}</div>
-                        <div>Type: {challenge.type}</div>
-                        <div>Created At: {challenge.createdAt}</div>
-                        <div>Updated At: {challenge.updatedAt}</div>
-                    </div>
-                )
-            })
-            };
+        <div className="user-profile-my-challenges">
+            <div className="user-profile-my-challenges-container">
+                <h1>This is challenges page</h1>
+                {allMyChallenges &&
+                    allMyChallenges.map((challenge) => {
+                        return (
+                            <div key={challenge.name + challenge.id}>
+                                <h1>Name: {challenge.name}</h1>
+                                <div>Id :{challenge.id}</div>
+                                <div>Author :{challenge.Author.userName}</div>
+                                <div>Author Id: {challenge.authorId}</div>
+                                <div>
+                                    Labels:{" "}
+                                    {challenge.Labels ? (
+                                        challenge.Labels.map((label) => {
+                                            return <li key={label.name}>{label.name}</li>;
+                                        })
+                                    ) : (
+                                        <h2>Labels Not Found</h2>
+                                    )}
+                                </div>
+                                <div>Boiler Plate: {challenge.boilerPlate}</div>
+                                <div>Repository Name: {challenge.repositoryName}</div>
+                                <div>Description: {challenge.description}</div>
+                                <div>State: {challenge.state}</div>
+                                <div>Type: {challenge.type}</div>
+                                <div>Created At: {challenge.createdAt}</div>
+                                <div>Updated At: {challenge.updatedAt}</div>
+                            </div>
+                        );
+                    })}
+                
+                <Link to="/profile">
+                    <Button
+                        // className={classes.userProfileBackToMyProfile}
+                        style={{ minWidth: 150 }}
+                        variant="contained"
+                        color="default"
+                    >
+                        back to My Profile
+                    </Button>
+                </Link>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default UserProfileLanding;
--- client/src/pages/UserProfile/UserProfileLanding.js
+++ client/src/pages/UserProfile/UserProfileLanding.js
@@ -1,18 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./UserProfileLanding.css";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles(() => ({
+    userProfileButton: {
+        margin: "15px",
+    },
+}));
 const UserProfileLanding = () => {
-
+    const classes = useStyles();
     return (
-        <div style={{ paddingTop: '200px' }} >
+        <div className="user-profile-landing">
             <h1>This is User Area</h1>
-            <Link to='/profile/MyChallenges' >
-                mychallenges
-            </Link>
-            <br />
-            <Link to='/profile/info' >
-                info
-            </Link>
+            <div className="buttons-user-profile-landing">
+                <Link to="/profile/MyChallenges">
+                    <Button
+                        className={classes.userProfileButton}
+                        style={{ minWidth: 150 }}
+                        variant="contained"
+                        color="default"
+                    >
+                        My Challenges
+                    </Button>
+                </Link>
+                <Link to="/profile/info">
+                    <Button
+                        className={classes.infoButton}
+                        style={{ minWidth: 150 }}
+                        variant="contained"
+                        color="default"
+                    >
+                        Info
+                    </Button>
+                </Link>
+            </div>
         </div>
     );
 };
