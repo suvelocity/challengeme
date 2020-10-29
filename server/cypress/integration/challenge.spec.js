a107323af448df83a6e8bcf160007ac7c231f722
LAPTOP-IDJ82BKU\micha <michael.byalsky@gmail.com>
2020-10-29 18:31:23 +0200

    githubTest

--- server/cypress/integration/challenge.spec.js
+++ server/cypress/integration/challenge.spec.js
@@ -2,7 +2,8 @@
 describe("challenge route", () => {
     beforeEach(() => {
         cy.fixture('users.json').as('usersData')
-        cy.generateToken()
+        cy.generateToken('@usersData[0]')
+        cy.log(message)
     })
     it("can start server", () => {
         cy.request("http://localhost:8080").then(response => {
--- server/helpers/handleGithubTokens.js
+++ server/helpers/handleGithubTokens.js
@@ -8,23 +8,23 @@ module.exports = async function handleGithubTokens(headers) {
     console.log('Actions Alredy Used - ', headers['x-ratelimit-used']);
     console.log('---------------------------------------------------------------------');
 
-    // try {
-    //     const allTokens = await GitToken.findAll({})
-    //     const tokensArray = allTokens.map(token => token.dataValues.token)
+    try {
+        const allTokens = await GitToken.findAll({})
+        const tokensArray = allTokens.map(token => token.dataValues.token)
 
-    //     console.log('BEFORE WE DO AMAZING', process.env.GITHUB_ACCESS_TOKEN);
+        console.log('BEFORE WE DO AMAZING', process.env.GITHUB_ACCESS_TOKEN);
 
-    //     let location = tokensArray.indexOf(process.env.GITHUB_ACCESS_TOKEN)
-    //     if (tokensArray.length === location + 1) {
-    //         location = -1
-    //     }
-    //     if (headers['x-ratelimit-remaining'] > 4500 ) {
-    //         process.env.GITHUB_ACCESS_TOKEN = tokensArray[location + 1];
-    //     }
+        let location = tokensArray.indexOf(process.env.GITHUB_ACCESS_TOKEN)
+        if (tokensArray.length === location + 1) {
+            location = -1
+        }
+        if (headers['x-ratelimit-remaining'] > 4500 ) {
+            process.env.GITHUB_ACCESS_TOKEN = tokensArray[location + 1];
+        }
 
-    //     console.log('AFTER SWITCH', process.env.GITHUB_ACCESS_TOKEN);
+        console.log('AFTER SWITCH', process.env.GITHUB_ACCESS_TOKEN);
 
-    // } catch (error) {
-    //     console.error(error);
-    // }
+    } catch (error) {
+        console.error(error);
+    }
 };
