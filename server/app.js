d503a80b6fbcdcbc3235faeb8bb507a7a07d15b5
david35008 <67857450+david35008@users.noreply.github.com>
2020-10-31 18:05:40 +0200

    add helmet

--- server/app.js
+++ server/app.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const path = require('path');
-const helmet = require("helmet");
-// app.use(helmet({ hsts: false, contentSecurityPolicy: false, referrerPolicy: false }));
-
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(require("./middleware/morgan"));
-
-// app.get("/", (req, res) => {
-//   res.json({ name: 'Jane' })
-// })
 
 app.use('/api', require('./api')); // if auth works this can probably go
 
