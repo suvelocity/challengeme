197d4619e9bed9e1233a06cd011c029735ab368b
michael byalsky <michael.byalsky@gmail.com>
2020-11-27 11:24:25 +0200

    all routes

--- server/app.js
+++ server/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const swaggerDocument = require('./swagger.json');
+const swaggerUi = require('swagger-ui-express');
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json({ limit: '50mb' }));
 app.use(require('./middleware/morgan'));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', require('./api'));
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
