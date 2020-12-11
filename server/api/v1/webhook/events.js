a94a79eb7af3ef13f21fe0c67e377731707fae70
david35008 <67857450+david35008@users.noreply.github.com>
2020-12-11 14:52:21 +0200

    add webhook routes
    authorization
    logout

--- server/api/v1/webhook/events.js
+++ server/api/v1/webhook/events.js
@@ -1,5 +1,6 @@
 const eventsWebhookRouter = require('express').Router();
 const axios = require('axios');
+const { Op } = require('sequelize');
 const { WebhookTeamEvent, Team } = require('../../../models');
 
 /* 
@@ -10,7 +11,7 @@ header : {
 body : {
     "teamId": "77d2ccb6-e6e2-4e85-92b2-73bf7c642adb",
     "webhookUrl": "http://localhost:8090/api/v1/webhook",
-    "events": {"SubmittedChallenge": "true"},
+    "events": {"submittedChallenge": "true"},
     "authorizationToken": "1234567abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 }
 */
@@ -19,30 +20,52 @@ body : {
 eventsWebhookRouter.post('/registration', async (req, res) => {
     const { teamId, webhookUrl, events, authorizationToken } = req.body
     try {
-        const teamExternalId = await Team.findOne({
+        const teamInsideId = await Team.findOne({
             where: {
                 externalId: teamId
             }
         })
-        // console.log(Object.assign(teamExternalId));
-        if (teamExternalId) {
+        if (teamInsideId) {
             try {
-
                 const response = await axios.post(webhookUrl, { message: 'test webhookUrl' }, {
                     headers: {
                         'Authorization': `token ${authorizationToken}`,
                     }
                 })
-                if (response) {
-                    const destructedRegistrationTeamWebhook = {
-                        teamId: teamId,
-                        teamInsideId: teamExternalId.id,
-                        webhookUrl: webhookUrl,
-                        events: events,
-                        authorizationToken
-                    };
-                    await WebhookTeamEvent.create(destructedRegistrationTeamWebhook);
+                if (response.statusText === 'OK') {
+                    const alreadyRegistered = await WebhookTeamEvent.findOne({
+                        where: {
+                            teamId,
+                            webhookUrl
+                        }
+                    })
+                    if (alreadyRegistered) {
+                        await WebhookTeamEvent.update({
+                            events: {
+                                ...alreadyRegistered.events,
+                                ...events
+                            }
+                        },
+                            {
+                                where: {
+                                    teamId,
+                                    webhookUrl
+                                },
+                            },
+                        )
+                    } else {
+                        const destructedRegistrationTeamWebhook = {
+                            teamId: teamId,
+                            teamInsideId: teamInsideId.id,
+                            webhookUrl: webhookUrl,
+                            events: events,
+                            authorizationToken
+                        };
+                        await WebhookTeamEvent.create(destructedRegistrationTeamWebhook);
+                    }
                     res.sendStatus(201);
+                } else {
+                    return res.status(400).json({ message: `webhookUrl ${webhookUrl} have a bad response` })
                 }
             } catch (error) {
                 console.error(error.message);
@@ -56,5 +79,97 @@ eventsWebhookRouter.post('/registration', async (req, res) => {
         res.status(400).json({ message: 'Cannot process request' });
     }
 });
+
+/*
+ header : {
+    Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6Ikp // webhook token
+}
+body : {
+    "webhookUrl": "http://localhost:8090/api/v1/webhook",
+    "authorizationToken": "1234567abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" // new one
+}
+ */
+
+// update authorization token
+eventsWebhookRouter.patch('/authorization', async (req, res) => {
+    const { webhookUrl, authorizationToken } = req.body
+    try {
+        const isWebhookExist = await WebhookTeamEvent.update({
+            authorizationToken,
+        },
+            {
+                where: {
+                    webhookUrl
+                },
+            })
+        if (isWebhookExist[0] > 0) {
+            res.json({ message: 'Update Authorization Token Success' })
+        } else {
+            res.status(400).json({ message: 'Update Authorization Token Fail' })
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).json({ message: 'Cannot process request' });
+    }
+});
+
+/* 
+request look like this :
+header : {
+    Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6Ikp // webhook token
+}
+body : {
+    "teamId": "77d2ccb6-e6e2-4e85-92b2-73bf7c642adb",
+    "webhookUrl": "http://localhost:8090/api/v1/webhook",
+      "events": {"StartedChallenge": "true", "submittedChallenge": "true"},
+    "authorizationToken": "1234567abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+}
+*/
+
+// update authorization token
+eventsWebhookRouter.delete('/logout', async (req, res) => {
+    const { teamId, events, webhookUrl } = req.body
+    try {
+
+        const isWebhookExist = await WebhookTeamEvent.findOne({
+            where: {
+                teamId,
+                webhookUrl,
+                // events: {
+                //     [Op.contains]: [{
+                //         ...events
+                //     }]
+                // }
+            }
+        })
+        if (isWebhookExist) {
+            const existEvents = { ...isWebhookExist.events }
+            Object.keys(events).forEach((eve => {
+                delete existEvents[eve]
+            }))
+            if (Object.keys(isWebhookExist.events).length === Object.keys(existEvents).length) {
+                return res.status(400).json({ message: 'You are not register with this events' })
+            } else {
+                await WebhookTeamEvent.update({
+                    events: existEvents
+                },
+                    {
+                        where: {
+                            teamId,
+                            webhookUrl
+                        },
+                    },
+                )
+            }
+            return res.json({ message: `Logout from ${Object.keys(events)} Events Success` })
+        } else {
+            return res.status(400).json({ message: 'You are not register with this team or webhookUrl' })
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).json({ message: 'Cannot process request' });
+    }
+});
+
 
 module.exports = eventsWebhookRouter;
