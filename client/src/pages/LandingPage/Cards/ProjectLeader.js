cbd839bfcd029141447d2c38cf721fcbde9bb01e
david35008 <67857450+david35008@users.noreply.github.com>
2020-12-22 23:21:10 +0200

    add Contributors

--- client/src/pages/LandingPage/Cards/ProjectLeader.js
+++ client/src/pages/LandingPage/Cards/ProjectLeader.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 
-export default function SimpleCard({ icon, name, rule, content }) {
+export default function SimpleCard({ icon, name, rule, content, github, linkedin }) {
     return (
         <Card className='Landing-page-Project-Leaders-Root'>
             <CardContent>
-                <Typography className='Landing-page-Project-Leaders-Icon' variant="h5" component="img" src={icon} />
-                <Typography className='Landing-page-Project-Leaders-Name' variant="h5" component="h2">{name}</Typography>
+                <IconButton href={`https://www.linkedin.com/in/${linkedin}`} className='Landing-page-Project-Leaders-Icon'>  <img className='Landing-page-Project-Leaders-Image' src={icon} /></IconButton>
+                <IconButton style={{margin: 0}} href={`https://github.com/${github}`}><Typography className='Landing-page-Project-Leaders-Name' variant="h5" component="h2">{name}</Typography></IconButton>
                 <Typography className='Landing-page-Project-Leaders-Rule' variant="body2" component="p">{rule}</Typography>
                 <Typography className='Landing-page-Project-Leaders-Content' variant="body2" component="p">{content}</Typography>
             </CardContent>
--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -6,6 +6,7 @@ import Title from './SvgComponents/Title';
 import Stripes from './SvgComponents/Stripes';
 import Section from './Cards/Section';
 import ProjectLeader from './Cards/ProjectLeader';
+import Contributor from './Cards/Contributor';
 import Student from './SvgComponents/Student';
 import Teams from './SvgComponents/Teams';
 import TeacherAnalytics from './SvgComponents/TeacherAnalytics';
@@ -49,6 +50,8 @@ export default function LandingPage() {
             rule: 'Project Main Leader',
             content: `As a developer who was looking to break into Tech, I knew the underlying logic of programming, but I had a lot of gaps in my understanding, especially on the types of algorithms questions asked at interviews. I can confidently say that ChallenMe is one of the best resources out there for challenge yourself and others, with fantastic video tutorials and an excellent question selection that allows you to get a deep understanding of the topics and confidence in your problem solving ability. The site is incredibly intuitive to use and I think the staff are some of the best out there, being incredibly supportive and passionate about offering a great customer experience. I can not recommend ChallemgeMe highly enough.
             `,
+            linkedin: 'davidiamant',
+            github: 'david35008'
         },
         {
             icon: 'https://media-exp1.licdn.com/dms/image/C4E35AQEUumDeyAuetw/profile-framedphoto-shrink_200_200/0/1604073201265?e=1608753600&v=beta&t=bKbFqRbwuOE9Arl85haenkiThpT6rWU5khFqU6X1qJk',
@@ -56,6 +59,8 @@ export default function LandingPage() {
             rule: 'Project Main Leader',
             content: `ChallengeMe has been the backbone of preparing my technical coding skills. This allows you to work efficiently on the most common variations of the highest WEB development without having to spend hours battling the algorithm just to arrive at an inefficient or incorrect solution. There are plenty of resources available for rehearsal, but ChallengeMe differentiates its product by providing the 'how' and 'why' in clear and concise videos. Developing a deeper understanding of how to approach these issues is better than trying to memorize lines of code. I highly recommend ChallengeMe.
             `,
+            linkedin: 'roy-shnitzer-1080671b7',
+            github: 'RoyShnitzel'
         },
         {
             icon: 'https://media-exp1.licdn.com/dms/image/C4D03AQHitfXfdK1h_Q/profile-displayphoto-shrink_200_200/0/1517597484023?e=1614211200&v=beta&t=QhESfo63QFM3E89UG-LQoSANWQXf3lgaDBeWYU4_2qk',
@@ -63,6 +68,26 @@ export default function LandingPage() {
             rule: 'Moderator',
             content: `I'm just writing to thank you for this product. I had failed in so many technologies before, but I wanted to get into a top tech company so much that I even enrolled in a Master's program. Even then, I was unsure if I had what it takes to make it. From the moment I heard your first video explanation, I thought 'this is exactly the way to learn new technology' (plus the extra points you can grab by asking clarifying questions). After a few months of studying, mainly on ChallengeMe , I got offers to intern at Microsoft and Google!
             ` ,
+            linkedin: 'guy-serfaty-aa34b413a',
+            github: 'GuySerfaty'
+        }
+    ]
+
+    const contributors = [
+        {
+            icon: 'https://media-exp1.licdn.com/dms/image/C4E35AQG0EjerwTpj_g/profile-framedphoto-shrink_200_200/0/1606295533050?e=1608757200&v=beta&t=-zIfQuTU9Oa6afmDkK2zHt70HoRcF7Diw_TW0ps6YYs',
+            name: 'Dor Kachlon',
+            github: 'DorKachlon',
+        },
+        {
+            icon: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
+            name: 'Alon Bruk',
+            github: 'AlonBru',
+        },
+        {
+            icon: 'https://media-exp1.licdn.com/dms/image/C4E03AQHC42ZPhrq_aQ/profile-displayphoto-shrink_200_200/0/1604991996965?e=1614211200&v=beta&t=CfAxKwq6pJVUrj9-5wadH8XAyKBRFrEpmXehmO0mafg',
+            name: 'Tsach Ovadia',
+            github: 'ItsDrTsach',
         }
     ]
 
@@ -117,22 +142,26 @@ export default function LandingPage() {
                 <h2 className='Landing-page-Project-Leaders-H2'>Project Leaders</h2>
                 <div className='Landing-page-Project-Leaders-Cards'>
                     {projectLeaders.map(elem =>
-                        < ProjectLeader icon={elem.icon} name={elem.name} rule={elem.rule} content={elem.content} />)
+                        < ProjectLeader
+                            icon={elem.icon}
+                            name={elem.name}
+                            rule={elem.rule}
+                            content={elem.content}
+                            linkedin={elem.linkedin}
+                            github={elem.github}
+                        />)
+                    }
+                </div>
+            </section>
+            <section className='Landing-page-Contributors' >
+                <h2 className='Landing-page-Contributors-H2'>Contributors</h2>
+                <div className='Landing-page-Contributors-Cards'>
+                    {contributors.map(elem =>
+                        < Contributor icon={elem.icon} name={elem.name} github={elem.github} />)
                     }
                 </div>
             </section>
 
-            {/* <div className='Heading-and-Buttons'></div> */}
-            {/* <div className='Card-explanation-section'></div> */}
-            {/* <div className='Auto-Layout'></div> */}
-            {/* <div className='Its-Open-Source'></div> */}
-            {/* <div className='Dont-limit-your-challenges-Challenge-your-Limits'></div> */}
-            {/* <div className='Auto-Layout1'></div> */}
-            {/* <div className='NavbarII'></div> */}
-            {/* <div className='Project-Leaders'></div> */}
-            {/* <div className='Contribute-Section'></div> */}
-            {/* <div className='image-5'></div> */}
-            {/* <div className='image-6'></div> */}
         </div>
     );
 }
