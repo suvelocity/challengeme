dc97757e8979414c888d78ad03bdd8d29667cff5
RoyShnitzel <67857836+RoyShnitzel@users.noreply.github.com>
2020-12-23 16:08:20 +0200

    update filters

--- client/src/components/Choosers/ChooseLabels.js
+++ client/src/components/Choosers/ChooseLabels.js
@@ -1,27 +1,30 @@
-import React, { useEffect } from 'react';
-import Selector from 'react-select';
-import network from '../../services/network';
-import './ChooseLabel.css';
+import React, { useEffect } from "react";
+import Selector from "react-select";
+import network from "../../services/network";
+import "./ChooseLabel.css";
 
 const ChooseLabels = ({
-  labels, chooseLabels, setChooseLabels, setLabels, darkMode,
+  labels,
+  chooseLabels,
+  setChooseLabels,
+  setLabels,
+  darkMode,
 }) => {
   useEffect(
     // gets existing labels
     () => {
       (async () => {
         try {
-          const { data } = await network.get('/api/v1/labels');
+          const { data } = await network.get("/api/v1/labels");
           const optionsForSelector = data.map((labelData) => ({
             value: labelData.id,
             label: labelData.name,
           }));
           setChooseLabels(optionsForSelector);
-        } catch (error) {
-        }
+        } catch (error) {}
       })();
     },
-    [setChooseLabels],
+    [setChooseLabels]
   );
 
   const selectionChange = (choosens) => {
@@ -31,17 +34,40 @@ const ChooseLabels = ({
   const customStyles = {
     option: (provided) => ({
       ...provided,
-      borderBottom: '1px dotted black',
-      color: darkMode ? 'white' : 'blue',
-      backgroundColor: darkMode ? 'rgb(51,51,51)' : 'white',
-      height: '100%',
+      border: "1px solid black",
+      borderRadius: "3px",
+      color: darkMode ? "white" : "black",
+      fontWeight: "bold",
+      // backgroundColor: darkMode ? "rgb(51,51,51)" : "white",
+      width: 100,
+      boxSizing: "border-box",
+      margin: 3,
+      backgroundColor: "rgba(30, 61, 91,0.5)",
+      ":hover": {
+        backgroundColor: "#1E3D5B",
+        color: "white",
+      },
+      minHeight: 60,
     }),
-    control: (provided) => (darkMode ? {
+    menu: (provided) => ({
       ...provided,
-      backgroundColor: 'neutral30',
-    } : {
-      ...provided,
+      width: 350,
+      paddingLeft: 10,
     }),
+    menuList: (provided) => ({
+      ...provided,
+      display: "flex",
+      flexWrap: "wrap",
+    }),
+    control: (provided) =>
+      darkMode
+        ? {
+            ...provided,
+            backgroundColor: "neutral30",
+          }
+        : {
+            ...provided,
+          },
   };
   return (
     <div className="labelFilter">
