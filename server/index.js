2a8e1a804190bab81e6f99bc05b9560f5815b473
david35008 <67857450+david35008@users.noreply.github.com>
2021-02-05 14:46:27 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
-
+const https = require('https');
+const fs = require('fs');
 const port = process.env.PORT || 80;
 const httpsPort = process.env.SECURE_PORT || 443;
 const spliceIpAddress = process.env.IP_ADDRESS.lastIndexOf(':')
@@ -12,10 +13,16 @@ if (env === 'development') {
   establishNgrokConnection();
 }
 
-app.listen(port, () => {
-  console.log(`ChallengeMe listening at ${ipAddress}${port}`);
-});
+if(env !== 'development') {
+const options = {
+ key: fs.readFileSync('./0018_key-certbot.pem'),
+  cert: fs.readFileSync('./0018_csr-certbot.pem')
+};
 
-app.listen(httpsPort, () => {
-  console.log(`Secure ChallengeMe listening at ${ipAddress}${httpsPort}`);
-});
+https.createServer(options, app).listen(443);
+
+}
+ app.listen(port, () => {
+  console.log(`ChallengeMe listening at ${ipAddress}${port}`);
+ });
+
