66d7b7081c1e3f89263fec7e3ba3f8c06aa72b02
david35008 <67857450+david35008@users.noreply.github.com>
2021-02-25 22:36:20 +0200

    parsing the right data

--- server/api/v1/insightsRoutes/mixpanel.js
+++ server/api/v1/insightsRoutes/mixpanel.js
@@ -1,4 +1,4 @@
-const insightMixpanelRoute = require("express").Router();
+const mixpanelRoute = require("express").Router();
 const {
   checkTeacherPermission,
   checkTeamPermission,
@@ -6,21 +6,37 @@ const {
 const { getTeamUsersNames } = require("./getTeamUsers");
 const axios = require("axios");
 
+function validNum(data) {
+  const parsedInt = parseInt(data)
+  if (!isNaN(parsedInt)) {
+    return Date.now()
+  } else {
+    parsedInt
+  }
+}
+
 // get all team users start challenge time of the last month
 // on development just team 3 have started events (suvelocity user)
-insightMixpanelRoute.get(
-  "/started-challenge/:teamId",
-    checkTeamPermission,
-    checkTeacherPermission,
-  async (req, res) => {
+mixpanelRoute.get("/started-challenge/:teamId",
+  checkTeamPermission, checkTeacherPermission, async (req, res) => {
+    const { teamId } = req.params;
+    const { from, to } = req.query;
     try {
-      const usersNames = await getTeamUsersNames(req.params.teamId);
-      todayDate = new Date().toISOString().slice(0,10);
+      const usersNames = await getTeamUsersNames(teamId);
+      const fromDate = validNum(from)
+      const toDate = validNum(to)
+      console.log(fromDate);
+      console.log(toDate);
+      const formattedFrom = new Date(fromDate).toISOString().slice(0, 10);
+      const formattedTo = new Date(toDate).toISOString().slice(0, 10);
+      todayDate = new Date().toISOString().slice(0, 10);
+      console.log('todayDate', todayDate);
       oneMonth = 30 * 24 * 60 * 60 * 1000;
-      oneMonthAgoDate = new Date(Date.now() - oneMonth).toISOString().slice(0,10);
+      oneMonthAgoDate = new Date(Date.now() - oneMonth).toISOString().slice(0, 10);
+      console.log('oneMonthAgoDate', oneMonthAgoDate);
       // the mixpanel return string !!
-      const { data } = await axios.get(
-        `https://data.mixpanel.com/api/2.0/export?to_date=${todayDate}&from_date=${oneMonthAgoDate}&event=%5B%22User%20Started%20Challenge%22%5D`,
+      const response = await axios.get(
+        `https://data.mixpanel.com/api/2.0/export?to_date=${formattedTo}&from_date=${formattedFrom}&event=%5B%22User%20Started%20Challenge%22%5D`,
         {
           headers: {
             "Content-Type": "application/json",
@@ -29,38 +45,21 @@ insightMixpanelRoute.get(
         }
       );
 
-      console.log("users", usersNames);
-
-      // orginized the string to arrays 
-      dataObjects = data.split("}");
-      dataStrings = dataObjects.map((str) => str.split(","));
-
-      // find the relevant data and push it to array of objects
-      let result = [];
-      dataStrings.forEach((arr) => {
-        let challengeId; 
-        arr.forEach((str) => {
-          let obj = {};
-          if(str.includes("ChallengeId")){
-            challengeId = str.split(':')[1].split('\"')[1];
-          }
-          usersNames.forEach((user) => {
-            if (str && str.includes(user)) {
-              obj.userName = user;
-              obj.time = arr[1].slice(-10);
-              obj.challengeId = challengeId
-              result.push(obj)
-            }
-          });
-        });
-      });
-
+      // // orginized the string to arrays 
+      const regex = /}}/ig
+      const commaEndOfObjects = response.data.replace(regex, '}},');
+      const lastCommaToRemove = commaEndOfObjects.lastIndexOf('}},') + 2
+      const removeLastComma = commaEndOfObjects.slice(0, lastCommaToRemove) + commaEndOfObjects.slice(lastCommaToRemove +1);
+      const onlySingleQuotes = removeLastComma.replace(/'/g, '"');
+      const insideArray = `[${onlySingleQuotes}]`
+      const result = JSON.parse(insideArray)
+        
       res.json(result);
     } catch (error) {
-      console.error(error);
+      console.error(error.message);
       res.status(400).json({ message: "Cannot process request" });
     }
   }
 );
 
-module.exports = insightMixpanelRoute;
+module.exports = mixpanelRoute;
