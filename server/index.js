5ed4be5569ecd0e723a99522a591dcc45ba7b22d
listguy <nitzanjl@gmail.com>
2021-04-26 20:04:17 +0300

    redploy

--- server/index.js
+++ server/index.js
@@ -1,25 +1,25 @@
-require('dotenv').config();
-const https = require('https');
-const fs = require('fs');
-const app = require('./app');
+require("dotenv").config();
+const https = require("https");
+const fs = require("fs");
+const app = require("./app");
 
 const port = process.env.PORT || 80;
-const spliceIpAddress = process.env.IP_ADDRESS.lastIndexOf(':');
+const spliceIpAddress = process.env.IP_ADDRESS.lastIndexOf(":");
 const ipAddress = process.env.IP_ADDRESS.substr(0, spliceIpAddress + 1);
-const env = process.env.NODE_ENV || 'development';
-const establishNgrokConnection = require('./helpers/ngrokConnection');
+const env = process.env.NODE_ENV || "development";
+const establishNgrokConnection = require("./helpers/ngrokConnection");
 
-if (env === 'development') {
+if (env === "development") {
   establishNgrokConnection();
 }
 
-if (env !== 'development') {
-  const options = {
-    key: fs.readFileSync('./private-key.pem'),
-    cert: fs.readFileSync('./certificate.pem'),
-  };
-  https.createServer(options, app).listen(443);
-}
+// if (env !== 'development') {
+//   const options = {
+//     key: fs.readFileSync('./private-key.pem'),
+//     cert: fs.readFileSync('./certificate.pem'),
+//   };
+//   https.createServer(options, app).listen(443);
+// }
 app.listen(port, () => {
   console.log(`ChallengeMe listening at ${ipAddress}${port}`);
 });
